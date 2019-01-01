bdbb73743f3ee4e1787f12e2405ae15663f645b4
makutak <takkouno@gmail.com>
2019-01-01 19:09:48 +0900

    ライセンス戻した

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject todo-list "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "MIT License"
-            :url "https://opensource.org/licenses/MIT"}
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
   :repl-options {:init-ns todo-list.core})
