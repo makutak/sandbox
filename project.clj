61f86ce07695b101b64d7c51ef6cec68f5c06212
makutak <takkouno@gmail.com>
2019-01-02 18:16:17 +0900

    main namespaceを追加

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [ring "1.7.1"]]
-  :repl-options {:init-ns todo-list.core})
+  :repl-options {:init-ns todo-list.core}
+  :main todo-list.core)
