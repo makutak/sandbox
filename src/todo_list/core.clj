8484cccef04ae4ffdf3003b1d7dffe6ff32fef69
makutak <takkouno@gmail.com>
2019-01-04 23:04:30 +0900

    main関数からレスポンスを分けた

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -4,13 +4,12 @@
 
 (defn welcome
   [request]
-  request)
+  {:status 200
+   :body "<h1>Hello, Clojure World</h1>  <p>Welcome to your first Clojure app.  This message is returned regardless of the request, sorry</p>"
+   :headers {}})
 
 (defn -main
   "A very simple web server using Ring & Jetty"
   [port-number]
-  (jetty/run-jetty
-   (fn [request] {:status 200
-                  :body "<h1>Hello, Clojure World</h1>  <p>Welcome to your first Clojure app.  This message is returned regardless of the request, sorry</p>"
-                  :headers {}})
-   {:port (Integer. port-number)}))
+  (jetty/run-jetty welcome
+                   {:port (Integer. port-number)}))
