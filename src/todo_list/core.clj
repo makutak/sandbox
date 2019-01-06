60ac2ef982da02f22f3dd7ca3b3107dc0b6e1cff
makutak <takkouno@gmail.com>
2019-01-06 23:33:59 +0900

    修正

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -7,14 +7,14 @@
   If a request is for something other than then an error message is returned"
   [request]
   (if (= "/" (:uri request))
-      {:status 200
-       :body "<h1>Hello, Clojure World</h1>
-              <p>Welcome to your first Clojure app.</p>"
-       :headers {}}
-      {:status 404
-       :body "<h1>This is not the page you are looking for</h1>
-             <p>Sorry, the page you requested was not found!</p>"
-       :headers {}}))
+    {:status 200
+     :body (str "<h1>Hello, Clojure World</h1>"
+                "<p>Welcome to your first Clojure app.</p>")
+     :headers {}}
+    {:status 404
+     :body (str "<h1>This is not the page you are looking for</h1>"
+                "<p>Sorry, the page you requested was not found!</p>")
+     :headers {}}))
 
 (defn -main
   "A very simple web server using Ring & Jetty"
