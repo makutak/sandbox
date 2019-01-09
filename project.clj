894dc58a3c7b54026a9862968471d5aebad4d769
makutak <takkouno@gmail.com>
2019-01-09 22:19:53 +0900

    add compojure

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [ring "1.7.1"]]
+                 [ring "1.7.1"]
+                 [compojure "1.6.1"]]
   :repl-options {:init-ns todo-list.core}
   :main todo-list.core
   :profiles {:dev
