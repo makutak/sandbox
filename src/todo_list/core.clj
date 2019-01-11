c3f20178c7d4b55182925d93afc1b8ce98a82a1a
makutak <takkouno@gmail.com>
2019-01-11 22:27:56 +0900

    use compojure to route

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -2,31 +2,32 @@
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
             [compojure.core :refer [defroutes GET]]
-            [compojure.route :refe [not-find]]))
+            [compojure.route :refer [not-found]]))
 
 (defn welcome
   "A ring handler to process all requests for the web server.
   If a request is for something other than then an error message is returned"
   [request]
-  (if (= "/" (:uri request))
-    {:status 200
-     :body (str "<h1>Hello, Clojure World</h1>"
-                "<p>Welcome to your first Clojure app, I now update automically</p>")
-     :headers {}}
-    {:status 404
-     :body (str "<h1>This is not the page you are looking for</h1>"
-                "<p>Sorry, the page you requested was not found!</p>")
-     :headers {}}))
+  {:status 200
+   :body (str "<h1>Hello, Clojure World</h1>"
+              "<p>Welcome to your first Clojure app, I now update automically</p>"
+              "<p>I now use defroutes to manage incoming requests</p>")
+   :headers {}})
+
+(defroutes app
+  (GET "/" [] welcome)
+  (not-found (str "<h1>This is not the page you are looking for</h1>"
+                "<p>Sorry, the page you requested was not found!</p>")))
 
 (defn -main
   "A very simple web server using Ring & Jetty"
   [port-number]
-  (jetty/run-jetty welcome
+  (jetty/run-jetty app
                    {:port (Integer. port-number)}))
 
 (defn -dev-main
   "A very simple web server using Ring & Jetty
   that reloads code changes via the development profile of Leiningen"
   [port-number]
-  (jetty/run-jetty (wrap-reload #'welcome)
+  (jetty/run-jetty (wrap-reload #'app)
                    {:port (Integer. port-number)}))
