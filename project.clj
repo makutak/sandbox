a2cfb49968da3a6b7c9495d80034e0b04103379e
makutak <takkouno@gmail.com>
2019-01-16 21:43:12 +0900

    fix typo

--- project.clj
+++ project.clj
@@ -10,6 +10,6 @@
   :repl-options {:init-ns todo-list.core}
   :main todo-list.core
   :min-lein-version "2.0.0"
-  :uberjar-name: "todo-list.jar"
+  :uberjar-name "todo-list.jar"
   :profiles {:dev
              {:main todo-list.core/-dev-main}})
