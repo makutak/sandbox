218da570bf240fa89e2827f622b634aee7573494
makutak <takkouno@gmail.com>
2019-01-22 22:50:46 +0900

    とりあえず書き始めた

--- web-server/modoki/src/modoki/core.clj
+++ web-server/modoki/src/modoki/core.clj
@@ -1,7 +1,16 @@
 (ns modoki.core
+  (:import [java.net ServerSocket])
+  (:require [clojure.java.io :as io])
   (:gen-class))
+
+(defn server
+  [port-number]
+  (with-open [server (ServerSocket. port-number)
+              socket (.accept server)
+              input (io/input-stream socket)]
+    (println input)))
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (server (Integer. 8001)))
