802634343e3b5b320cbd5375ddb345131700dba2
makutak <takkouno@gmail.com>
2019-01-24 22:45:39 +0900

    SocketInputStreamを読むにはこうするしかないのか

--- web-server/modoki/src/modoki/core.clj
+++ web-server/modoki/src/modoki/core.clj
@@ -1,20 +1,26 @@
 (ns modoki.core
   (:import [java.net ServerSocket])
+  (:import [java.io PrintStream])
   (:require [clojure.java.io :as io])
+  (:refer-clojure :exclude [read-line])
   (:gen-class))
+
+(defn read-line
+  [input]
+  )
+
 
 (defn server
   [port-number]
   (println "wait for connect..")
   (with-open [server (ServerSocket. port-number)
               socket (.accept server)
-              input (io/reader socket)]
-    (println "connect!!")
-    (loop [r (.readLine input)]
-      (when (not (nil? r))
-        (println r)
-        (recur (.readLine input))))
-    (.close socket)))
+              input (.getInputStream socket)]
+    (println"connect!!")
+    (loop [ch (.read input)]
+      (when-not (= ch 0)
+        (println ch)
+        (recur (.read input))))))
 
 (defn -main
   []
