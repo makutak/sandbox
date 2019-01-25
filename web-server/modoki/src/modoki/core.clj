cff0ca4438e55db16849af8bfa2d9f77df57c70b
makutak <takkouno@gmail.com>
2019-01-25 22:22:22 +0900

    clojureっぽい書き方

--- web-server/modoki/src/modoki/core.clj
+++ web-server/modoki/src/modoki/core.clj
@@ -1,26 +1,34 @@
 (ns modoki.core
   (:import [java.net ServerSocket])
-  (:import [java.io PrintStream])
+  (:import [java.io InputStream])
   (:require [clojure.java.io :as io])
   (:refer-clojure :exclude [read-line])
   (:gen-class))
 
-(defn read-line
-  [input]
-  )
 
+
+(defn read-line
+  [^InputStream input-stream]
+  (let [input (.read input-stream)]
+    (if-not (= input -1)
+      (cons input (lazy-seq (read-line input-stream))))))
 
 (defn server
   [port-number]
   (println "wait for connect..")
   (with-open [server (ServerSocket. port-number)
               socket (.accept server)
-              input (.getInputStream socket)]
+              input (io/input-stream socket)
+              output (io/output-stream "request.txt")]
     (println"connect!!")
-    (loop [ch (.read input)]
-      (when-not (= ch 0)
-        (println ch)
-        (recur (.read input))))))
+    ;;(println (read-line input))
+    (dorun (map #(.write output %) (read-line input)))
+    ;; (loop [ch (.read input)]
+    ;;   (when-not (= ch -1)
+    ;;     (.write output ch)
+    ;;     (recur (.read input))))
+    ;; (println "done")
+    (.close socket)))
 
 (defn -main
   []
