b68d4d1b0643dadd33cb10d85dd2449b7e670ca2
makutak <takkouno@gmail.com>
2019-01-27 03:21:56 +0900

    一瞬ブラウザで表示できるようになった。ステータス200も認識してる

--- web-server/modoki/src/modoki/core.clj
+++ web-server/modoki/src/modoki/core.clj
@@ -1,6 +1,7 @@
 (ns modoki.core
   (:import [java.net ServerSocket])
   (:import [java.io InputStream OutputStream])
+  (:import [java.io InputStream OutputStream])
   (:require [clojure.java.io :as io])
   (:refer-clojure :exclude [read-line write-line])
   (:gen-class))
@@ -13,9 +14,14 @@
 ;;     (if-not (= input -1)
 ;;       (cons input (lazy-seq (read-line input-stream))))))
 
-;; (defn write-line
-;;   [^OutputStream output-stream string]
-;;   (map #(.write output-stream (int %)) (char-array string)))
+(defn write-line
+  [^OutputStream output-stream string]
+  ;;(map #(.flush (.write output-stream (int %))) (char-array string))
+  (doseq [ch (char-array string)]
+    (.write output-stream (int ch)))
+  (.write output-stream (int \return))
+  (.write output-stream (int \newline))
+  (.flush output-stream))
 
 (defn server
   [port-number]
@@ -27,12 +33,23 @@
     (println"connect!!")
     ;;(dorun (map #(.write output %) (read-line input)))
     ;; (dorun (lazy-seq (write-line output "test")))
-    (loop [ch (.read input)]
-      (when-not (= ch -1)
-        (.write output ch)
-        (.flush output)
-        (recur (.read input))))
-    (println "done")
+    ;; (loop [ch (.read input)]
+    ;;   (when (not (= ch -1))
+    ;;     (recur (.read input))))
+    ;; (println "loop done")
+
+    ;; header
+    (write-line output "HTTP/1.1 200 OK")
+    (write-line output "Date: Sat, 26 Jan 2019 17:35:18 GMT")
+    (write-line output "Server: modoki")
+    (write-line output "Connection: close")
+    (write-line output "Content-type: text/html")
+    (write-line output "")
+    ;; body
+    (try
+      (write-line output "<h1>It works</1>")
+      (catch Exception e
+        (.printStackTrace e)))
     (.close socket)))
 
 (defn -main
