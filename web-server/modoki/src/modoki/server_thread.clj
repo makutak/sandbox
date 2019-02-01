d4d4135cc821206d6213b6ff7448dbdeacc699bd
makutak <takkouno@gmail.com>
2019-02-01 20:46:46 +0900

    ファイル分けた

--- web-server/modoki/src/modoki/server_thread.clj
+++ web-server/modoki/src/modoki/server_thread.clj
@@ -1,55 +1,12 @@
 (ns modoki.server_thread
   (:import [java.net Socket]
-           [java.io InputStream OutputStream FileInputStream]
-           [java.util Date Calendar TimeZone Locale]
-           [java.text SimpleDateFormat])
+           [java.io FileInputStream])
   (:require [clojure.java.io :as io]
-            [clojure.string :as s])
+            [clojure.string :as s]
+            [modoki.util :refer :all])
   (:refer-clojure :exclude [read-line write-line]))
 
 (def document-root "./resources")
-
-(def content-type-map {:html "text/html"
-                       :htm "text/html"
-                       :css "text/css"
-                       :png "image/png"
-                       :jpg "image/jpeg"
-                       :jpeg "image/jpeg"
-                       :gif "image/gif"})
-
-(defn get-content-type
-  [ext]
-  ((keyword ext) content-type-map "application/octet-stream"))
-
-(defn read-line
-  [^InputStream input-stream]
-  (let [input (.read input-stream)]
-    (if (and (not= input -1)
-             (not= input (int \newline)))
-      (cons input (read-line input-stream)))))
-
-(defn write-line
-  [^OutputStream output-stream string]
-  (doseq [ch (char-array string)]
-    (.write output-stream (int ch)))
-  (.write output-stream (int \return))
-  (.write output-stream (int \newline))
-  (.flush output-stream))
-
-(defn bytes->str
-  [lat]
-  (apply str (map #(char %) lat)))
-
-(defn get-path
-  [request-line]
-  (second (s/split request-line #" ")))
-
-(defn get-date-string-utc
-  []
-  (let [cal (Calendar/getInstance (TimeZone/getTimeZone "GMT"))
-        df (SimpleDateFormat. "EEE, dd MMM yyyy HH:mm:ss" Locale/US)]
-    (.setTimeZone df (.getTimeZone cal))
-    (str  (.format df (.getTime cal)) " GMT")))
 
 (defn server-thread
   [^Socket socket]
