3393c30c4d98538915efecf3061e678a4f851c71
makutak <takkouno@gmail.com>
2019-02-13 22:07:14 +0900

    ディレクトリトラバーサルに対応

--- web-server/modoki/src/modoki/server_thread.clj
+++ web-server/modoki/src/modoki/server_thread.clj
@@ -1,22 +1,24 @@
 (ns modoki.server_thread
   (:import [java.net Socket]
            [java.io FileInputStream FileNotFoundException]
-           [java.nio.file FileSystems Files LinkOption])
+           [java.nio.file FileSystems Files LinkOption NoSuchFileException])
   (:require [clojure.java.io :as io]
             [clojure.string :as s]
             [modoki.util :refer :all]
             [modoki.send_response :refer :all])
   (:refer-clojure :exclude [read-line write-line]))
 
-(def document-root "./resources")
+(def relative-document-root "./resources")
 (def error-document-root "./resources/error")
 (def server-name "localhost:8001")
+(def file-system (FileSystems/getDefault))
+(def document-root
+  (let [relative-path (.getPath file-system relative-document-root (into-array [""]))]
+    (str (.toRealPath relative-path (into-array LinkOption [])))))
 
 (defn build-location
   [host path]
   (str "http://" (if (nil? host) server-name host) path))
-
-
 
 (defn server-thread
   [^Socket socket]
@@ -26,22 +28,27 @@
           request-line (bytes->str (read-line input))
           path (get-request-path request-line)
           ext (get-ext path)
-          host (get-host (bytes->str (read-line input)))]
+          host (get-host (bytes->str (read-line input)))
+          fs (FileSystems/getDefault)
+          path-obj (.getPath fs (str document-root path) (into-array [""]))
+          ]
       (println "request line: " request-line)
       (println "path: " path " "
                "ext: " ext " "
                "host: " host)
       (try
-        (let [fis (io/input-stream (FileInputStream. (str document-root path)))
-              ]
-          (send-ok-response output fis ext))
-        (catch FileNotFoundException ex
-          (let [fs (FileSystems/getDefault)
-                path-obj (.getPath fs (str document-root path) (into-array [""]))
-                location (build-location host (redirect-path path))]
-            (if (Files/isDirectory path-obj (into-array LinkOption []))
-              (send-move-permanently-response output location)
-              (send-not-found-response output error-document-root))))))
+        (let [real-path (.toRealPath path-obj (into-array LinkOption []))
+              location (build-location host (redirect-path path))]
+          (cond
+            (not (s/starts-with? real-path document-root)) (send-not-found-response output error-document-root)
+            (Files/isDirectory real-path (into-array LinkOption [])) (send-move-permanently-response output location)
+            :else  (try
+                     (let [fis (io/input-stream (FileInputStream. (str document-root path)))]
+                       (send-ok-response output fis ext))
+                     (catch FileNotFoundException ex
+                       (send-not-found-response output error-document-root)))))
+        (catch NoSuchFileException ex
+          (send-not-found-response output error-document-root))))
     (catch Exception e
       (.printStackTrace e))
     (finally
