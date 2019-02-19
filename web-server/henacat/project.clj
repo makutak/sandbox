856841ed44da41723c62a63db7e8ce5de4afda2d
makutak <takkouno@gmail.com>
2019-02-19 21:09:04 +0900

    コンパイル時にクラスパスを通したい

--- web-server/henacat/project.clj
+++ web-server/henacat/project.clj
@@ -8,6 +8,7 @@
   :target-path "target/%s"
   :aot [henacat.test_BBS
         henacat.message]
+  :resource-paths ["lib/servlet-api.jar"]
   :profiles
   {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]]}
    :uberjar {:aot :all}})
