abe44979ae5595861a9a2fcb6b45d081dccb68ef
makutak <takkouno@gmail.com>
2019-02-20 22:41:37 +0900

    表示させたい

--- web-server/henacat/src/henacat/test_BBS.clj
+++ web-server/henacat/src/henacat/test_BBS.clj
@@ -6,4 +6,12 @@
 
 (defn -doGet
   [^HttpServletRequest request ^HttpServletResponse response]
-  (.setContentType response "text/html;charset=UTF-8"))
+  (.setContentType response "text/html;charset=UTF-8")
+  (let [out (.getWriter response)]
+    (.println out "<html>")
+    (.println out "<head>")
+    (.println out "</head>")
+    (.println out "<body>")
+    (.println out "<h1>hello world</h1>")
+    (.println out "</body>")
+    (.println out "</html>")))
