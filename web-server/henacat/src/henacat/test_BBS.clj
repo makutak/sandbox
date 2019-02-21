2f2954830dfefeecbb1580058bdbfa82f193bef8
makutak <takkouno@gmail.com>
2019-02-21 22:52:06 +0900

    wakaran

--- web-server/henacat/src/henacat/test_BBS.clj
+++ web-server/henacat/src/henacat/test_BBS.clj
@@ -2,9 +2,10 @@
   (:import [javax.servlet.http HttpServlet HttpServletRequest HttpServletResponse])
   (:gen-class
    :name henacat.TestBBS
-   :extends javax.servlet.http.HttpServlet))
+   :extends javax.servlet.http.HttpServlet
+   :exposes-methods [doGet]))
 
-(defn -doGet
+(defn- -doGet
   [^HttpServletRequest request ^HttpServletResponse response]
   (.setContentType response "text/html;charset=UTF-8")
   (let [out (.getWriter response)]
