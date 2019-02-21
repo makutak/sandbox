ea3a0e6b03dd6f5da5381535c5f57650684272ad
makutak <takkouno@gmail.com>
2019-02-22 03:03:46 +0900

    修正

--- web-server/henacat/src/henacat/test_BBS.clj
+++ web-server/henacat/src/henacat/test_BBS.clj
@@ -2,11 +2,10 @@
   (:import [javax.servlet.http HttpServlet HttpServletRequest HttpServletResponse])
   (:gen-class
    :name henacat.TestBBS
-   :extends javax.servlet.http.HttpServlet
-   :exposes-methods [doGet]))
+   :extends javax.servlet.http.HttpServlet))
 
-(defn- -doGet
-  [^HttpServletRequest request ^HttpServletResponse response]
+(defn -doGet
+  [this ^HttpServletRequest request ^HttpServletResponse response]
   (.setContentType response "text/html;charset=UTF-8")
   (let [out (.getWriter response)]
     (.println out "<html>")
