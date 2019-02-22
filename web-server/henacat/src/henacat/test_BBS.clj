6e373db475a6462e7ebf86ba9bffd80329e7aaf6
makutak <takkouno@gmail.com>
2019-02-23 01:49:36 +0900

    戻した

--- web-server/henacat/src/henacat/test_BBS.clj
+++ web-server/henacat/src/henacat/test_BBS.clj
@@ -2,11 +2,10 @@
   (:import [javax.servlet.http HttpServlet HttpServletRequest HttpServletResponse])
   (:gen-class
    :name TestBBS
-   :main false
    :extends javax.servlet.http.HttpServlet))
 
-(defn -servlet
-  [this ^HttpServletRequest request ^HttpServletResponse response]
+(defn -doGet
+  [this request response]
   (.setContentType response "text/html;charset=UTF-8")
   (let [out (.getWriter response)]
     (.println out "<html>")
@@ -16,8 +15,3 @@
     (.println out "<h1>hello world</h1>")
     (.println out "</body>")
     (.println out "</html>")))
-
-
-(defn -doGet
-  [this ^HttpServletRequest request ^HttpServletResponse response]
-  (-servlet this request response))
