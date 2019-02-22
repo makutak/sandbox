5e08286e0d0b7f064a674b490536125769823f80
makutak <takkouno@gmail.com>
2019-02-22 23:51:44 +0900

    ひとまず

--- web-server/henacat/src/henacat/test_BBS.clj
+++ web-server/henacat/src/henacat/test_BBS.clj
@@ -1,8 +1,14 @@
 (ns henacat.test_BBS
   (:import [javax.servlet.http HttpServlet HttpServletRequest HttpServletResponse])
   (:gen-class
-   :name henacat.TestBBS
+   :name TestBBS
+   :main false
    :extends javax.servlet.http.HttpServlet))
+
+(defn -servlet
+  [this ^HttpServletRequest request ^HttpServletResponse response]
+  )
+
 
 (defn -doGet
   [this ^HttpServletRequest request ^HttpServletResponse response]
