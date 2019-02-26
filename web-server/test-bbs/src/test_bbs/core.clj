29eeaab00a65f20a1100549131e9b7e07e59eb4b
makutak <takkouno@gmail.com>
2019-02-26 22:22:09 +0900

    これもだめ

--- web-server/test-bbs/src/test_bbs/core.clj
+++ web-server/test-bbs/src/test_bbs/core.clj
@@ -1,6 +1,15 @@
-(ns test-bbs.core)
+(ns test-bbs.core
+  (:import [javax.servlet.http.HttpServlet])
+  (:gen-class
+   :main false
+   :extends javax.servlet.http.HttpServlet))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -servlet
+  [this request response]
+  (.setContentType response "text/html; charset=UTF-8")
+  (let [out (.getWriter response)]
+    (.. out
+        (println "<html><body>hello world</body></html>"))))
+
+(defn -doGet [this request response]
+  (-servlet this request response))
