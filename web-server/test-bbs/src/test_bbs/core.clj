0f6e1914364288760227fc49e8253bb09203ad77
makutak <takkouno@gmail.com>
2019-02-27 21:03:27 +0900

    不要になった

--- web-server/test-bbs/src/test_bbs/core.clj
+++ web-server/test-bbs/src/test_bbs/core.clj
@@ -4,17 +4,8 @@
    :main false
    :extends javax.servlet.http.HttpServlet))
 
-(defn -init
-  [this request response]
-  [this]
-  (println "init"))
-
 (defn -doGet [this request response]
   (.setContentType response "text/html; charset=UTF-8")
   (let [out (.getWriter response)]
     (.. out
         (println "<html><body>hello world</body></html>"))))
-
-(defn -destroy
-  [this]
-  (println "destory"))
