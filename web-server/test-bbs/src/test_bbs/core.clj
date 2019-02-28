60101af2723e04263a919b760d1d9e6a9fadad59
makutak <takkouno@gmail.com>
2019-02-28 21:16:24 +0900

    外観

--- web-server/test-bbs/src/test_bbs/core.clj
+++ web-server/test-bbs/src/test_bbs/core.clj
@@ -1,11 +1,24 @@
 (ns test-bbs.core
   (:import [javax.servlet.http.HttpServlet])
   (:gen-class
+   :name TestBBS
    :main false
    :extends javax.servlet.http.HttpServlet))
 
 (defn -doGet [this request response]
   (.setContentType response "text/html; charset=UTF-8")
   (let [out (.getWriter response)]
-    (.. out
-        (println "<html><body>hello world</body></html>"))))
+    (.println out "<html>")
+    (.println out "<head>")
+    (.println out "<title>テスト掲示板</title>")
+    (.println out "</head>")
+    (.println out "<body>")
+    (.println out "<h1>テスト掲示板</h1>")
+    (.println out "<form action='/test-bbs/TestBBS' method=POST>")
+    (.println out "ハンドル名: <input type='text' name='handle'><br/>")
+    (.println out "<textarea name='message' rows='4' cols='60'></textarea><br/>")
+    (.println out "<input type='submit' />")
+    (.println out "</form>")
+    (.println out "<hr />")
+    (.println out "</body>")
+    (.println out "</html>")))
