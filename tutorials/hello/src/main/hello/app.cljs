aeef484aab94db0aca5074da866e7db3f63ff4c3
makutak <takkouno@gmail.com>
2019-03-06 21:25:25 +0900

    リクエストを送ってみる

--- tutorials/hello/src/main/hello/app.cljs
+++ tutorials/hello/src/main/hello/app.cljs
@@ -1,9 +1,14 @@
-(ns hello.app)
+(ns hello.app
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [cljs.core.async :refer [<!]]
+            [cljs-http.client :as http]))
+
+(defn test-request
+  []
+  (go (let [response (<! (http/get "https://example.com"))]
+        (prn (:status response)))))
+
 
 (defn -main
   [& args]
   (println args))
-
-(defn foo
-  []
-  (println "foo"))
