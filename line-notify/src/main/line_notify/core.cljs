53d2a666cf955b4a885391d1673849a5d0fb4766
makutak <takkouno@gmail.com>
2019-03-11 02:21:28 +0900

    ヘルプをひとまず

--- line-notify/src/main/line_notify/core.cljs
+++ line-notify/src/main/line_notify/core.cljs
@@ -3,12 +3,28 @@
   (:require ["xhr2" :as xhr2]
             [cljs.core.async :refer [<!]]
             [cljs-http.client :as http]
-            [clojure.tools.cli :refer [parse-opts]]))
+            [clojure.tools.cli :refer [parse-opts]]
+            [clojure.string :as s]))
+
 (def cli-options
   [["-h" "--help"]
    ["-m" "--message"]
    ["-c" "--config"]])
 
+
+(defn usage
+  [options-summary]
+  (s/join \newline
+          ["This is LIne Notify."
+           ""
+           "Usage: Line Notify [options] action."
+           "Options:"
+           options-summary
+           ""]))
+
 (defn -main
   [& args]
-  (parse-opts args cli-options))
+  (let [{:keys [options summary]} (parse-opts args cli-options)]
+    (cond
+      (:help options) (println (usage summary))
+      :else (println "this is line-notify!"))))
