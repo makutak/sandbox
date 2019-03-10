a26330f37956e2dc2bdf0f512fd82c99159d9263
makutak <takkouno@gmail.com>
2019-03-10 23:22:28 +0900

    引数の設定をしたい

--- line-notify/src/main/line_notify/core.cljs
+++ line-notify/src/main/line_notify/core.cljs
@@ -2,12 +2,13 @@
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require ["xhr2" :as xhr2]
             [cljs.core.async :refer [<!]]
-            [cljs-http.client :as http]))
+            [cljs-http.client :as http]
+            [clojure.tools.cli :refer [parse-opts]]))
+(def cli-options
+  [["-h" "--help"]
+   ["-m" "--message"]
+   ["-c" "--config"]])
 
 (defn -main
   [& args]
-  (cond
-    (= (first args) "--help") (prn "help")
-    (= (first args) "--config") (prn "config")
-    (= (first args) "--message") (prn "message")
-    :else (prn "see help")))
+  (parse-opts args cli-options))
