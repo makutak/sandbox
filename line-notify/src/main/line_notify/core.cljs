8703e972ef90b6d8b9867c36adb98cb91725db2c
makutak <takkouno@gmail.com>
2019-03-12 22:25:53 +0900

    コマンドライン引数からメッセージを送れる

--- line-notify/src/main/line_notify/core.cljs
+++ line-notify/src/main/line_notify/core.cljs
@@ -5,12 +5,12 @@
             [cljs-http.client :as http]
             [clojure.tools.cli :refer [parse-opts]]
             [clojure.string :as s]))
+(set! js/XMLHttpRequest xhr2)
 
 (def cli-options
   [["-h" "--help" "Show help"]
    ["-m" "--message" "You want to send message"]
-   ["-c" "--config" "Your LINE API Token"]])
-
+   ["-t" "--token" "Your LINE API Token"]])
 
 (defn usage
   [options-summary]
@@ -22,16 +22,48 @@
            options-summary
            ""]))
 
-(defn notify
+(defn parse-args
+  [args]
+  (map (fn [arg]
+         (cond
+           (or (= arg "-m") (= arg "--message")) :message
+           (or (= arg "-t") (= arg "--token")) :token
+           :else arg))
+       args))
+
+(defn split-args
+  [args]
+  (partition 2 (parse-args args)))
+
+(defn arg-map
+  [args]
+  (into {} (map vec (split-args args))))
+
+(defn send-message
   [token message]
-  (println token message))
+  (go (let [response (<! (http/post "https://notify-api.line.me/api/notify"
+                                    {:with-credentials? false?
+                                     :headers {"Authorization"
+                                               (str "Bearer "
+                                                    token)}
+                                     :form-params {:message message}}))]
+        (prn (:status response))
+        (prn (:body response)))))
+
+(defn notify
+  [args]
+  (let [config (arg-map args)
+        token (:token config)
+        message (:message config)]
+    (println config)
+    (println token)
+    (println message)
+    (send-message token message)))
 
 (defn -main
   [& args]
   (let [{:keys [options summary arguments]} (parse-opts args cli-options)]
-    (println  (parse-opts args cli-options))
-    (println arguments)
     (cond
       (:help options) (println (usage summary))
-      (and (:config options) (:message options)) (notify (:config options) (:message options))
+      (and (:token options) (:message options)) (notify args)
       :else (println "this is line-notify!"))))
