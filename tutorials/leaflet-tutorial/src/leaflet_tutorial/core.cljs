2ddaaac492f2cfffd72c46b67b59f8098bcc37a6
makutak <takkouno@gmail.com>
2019-03-28 04:01:23 +0900

    修正

--- tutorials/leaflet-tutorial/src/leaflet_tutorial/core.cljs
+++ tutorials/leaflet-tutorial/src/leaflet_tutorial/core.cljs
@@ -1,38 +1,29 @@
 (ns leaflet-tutorial.core
   (:require [reagent.core :as r]
-            [react-leaflet :as leaflet]))
+            [react-leaflet :refer [Map TileLayer Marker Popup]]))
 
-
-(def Map (r/adapt-react-class leaflet/Map))
-(def TileLayer (r/adapt-react-class leaflet/TileLayer))
-(def Marker (r/adapt-react-class leaflet/Marker))
-(def Popup (r/adapt-react-class leaflet/Popup))
 
 (def position [51.505 -0.09])
 
 
-(defn leaflet-map
-  []
-  [Map {:center position :zoom 13}
-   [TileLayer {:url "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-               :attribution "&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"}]
-   [Marker {:position position}
-    [Popup "A pretty CSS3 popup.<br />Easily customizable." ]]])
-
 ;; -------------------------
 ;; Views
-
-
-(defn home-page
+(defn leaflet-map
   []
-  [leaflet-map])
+  [:> Map {:center position :zoom 13}
+   [:> TileLayer {:url "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    :attribution "&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"}]
+   [:> Marker {:position position}
+    [:> Popup "A pretty CSS3 popup.<br />Easily customizable." ]]])
+
+
 
 ;; -------------------------
 ;; Initialize app
 
 (defn mount-root
   []
-  (r/render [home-page] (.getElementById js/document "app")))
+  (r/render [leaflet-map] (.getElementById js/document "app")))
 
 (defn init!
   []
