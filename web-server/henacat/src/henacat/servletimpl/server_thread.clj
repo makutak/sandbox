09357a0a0a6dffc3f62491376c95fc0c1e4dfb13
makutak <takkouno@gmail.com>
2019-03-30 22:42:25 +0900

    リクエストラインを取得

--- web-server/henacat/src/henacat/servletimpl/server_thread.clj
+++ web-server/henacat/src/henacat/servletimpl/server_thread.clj
@@ -1,13 +1,18 @@
 (ns henacat.servletimpl.server_thread
   (:import [java.net Socket])
   (:require [clojure.java.io :as io]
-            [clojure.string :as s])
-  (:refer-clojure :exclude [read-line write-line]))
+            [clojure.string :as s]
+            [henacat.util.util :as util]))
 
 (defn server-thread
   [^Socket socket]
   (try
-    (println socket)
+    (let [input (io/input-stream socket)
+          request-line (util/bytes->str (util/read-line input))
+          ;; TODO: request methodを取得
+
+          ])
+    (println request-line)
     (catch Exception e
       (.printStackTrace e))
     (finally
