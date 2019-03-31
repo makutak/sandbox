dfd2d218357192fa07ea20da3339f63ce63d4767
makutak <takkouno@gmail.com>
2019-03-31 23:25:32 +0900

    リクエストURLを取得する

--- web-server/henacat/src/henacat/servletimpl/server_thread.clj
+++ web-server/henacat/src/henacat/servletimpl/server_thread.clj
@@ -1,18 +1,22 @@
 (ns henacat.servletimpl.server_thread
-  (:import [java.net Socket])
+  (:import [java.net Socket URLDecoder])
   (:require [clojure.java.io :as io]
             [clojure.string :as s]
             [henacat.util.util :as util]))
+
+(def default-char-set "UTF-8")
 
 (defn server-thread
   [^Socket socket]
   (try
     (let [input (io/input-stream socket)
           request-line (util/bytes->str (util/read-line input))
+          path (URLDecoder/decode (util/get-request-path request-line) default-char-set)
           ;; TODO: request methodを取得
 
-          ])
-    (println request-line)
+          ]
+      (println request-line)
+      (println path))
     (catch Exception e
       (.printStackTrace e))
     (finally
