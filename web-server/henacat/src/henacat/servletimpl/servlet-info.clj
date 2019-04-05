9ad398fbc2b17e834400990350259ebd1e93a0b3
makutak <takkouno@gmail.com>
2019-04-05 20:46:22 +0900

    defrecord使うようにした

--- web-server/henacat/src/henacat/servletimpl/servlet-info.clj
+++ web-server/henacat/src/henacat/servletimpl/servlet-info.clj
@@ -2,14 +2,14 @@
   (:import [java.util HashMap]))
 
 (def servlet-collection (HashMap.))
-(def servlet-info (HashMap.))
+
+(defrecord ServletInfo [url-pattern servlet-directory servlet-className servlet])
 
 (defn make-servlet-info
   [url-pattern servlet-directory servlet-classname]
-  (.put servlet-info url-pattern url-pattern)
-  (.put servlet-info servlet-directory servlet-directory)
-  (.put servlet-info servlet-classname servlet-classname)
-  servlet-info)
+  (.put servlet-collection
+        url-pattern
+        (new ServerInfo url-pattern servlet-directory servlet-classname nil)))
 
 (defn add-servlet
   [url-pattern servlet-directory servlet-classname]
