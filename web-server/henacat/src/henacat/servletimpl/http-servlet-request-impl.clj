ca0194c191782d08aad562d10d35bcdff811d2be
makutak <takkouno@gmail.com>
2019-04-08 22:22:09 +0900

    ひとまず

--- web-server/henacat/src/henacat/servletimpl/http-servlet-request-impl.clj
+++ web-server/henacat/src/henacat/servletimpl/http-servlet-request-impl.clj
@@ -1,1 +1,10 @@
-(ns henacat.servletimpl.http-servlet-request-imple)
+(ns henacat.servletimpl.http-servlet-request-impl
+  (:import [javax.servlet.http.HttpServlet])
+  (:gen-class
+   :name henacat.servletimpl.http-servlet-request-impl
+   :main false
+   :implements [javax.servlet.http.HttpServlet]))
+
+(defn -getMethod
+  [this]
+  (.method this))
