61102325ccee69335d8923dd049f48f37f32fb6f
makutak <takkouno@gmail.com>
2019-04-12 22:37:41 +0900

    定数名の修正とレコードの定義

--- web-server/henacat/src/henacat/servletimpl/http_servlet_response_impl.clj
+++ web-server/henacat/src/henacat/servletimpl/http_servlet_response_impl.clj
@@ -1,4 +1,6 @@
 (ns henacat.servletimpl.http_servlet_response_impl)
 
-(def content-type "application/octet-stream")
-(def character-encoding "UTF-8")
+(def default-content-type "application/octet-stream")
+(def default-character-encoding "UTF-8")
+
+(defrecord HttpServletResponseImpl [content-type character-encoding output-stream print-writer])
