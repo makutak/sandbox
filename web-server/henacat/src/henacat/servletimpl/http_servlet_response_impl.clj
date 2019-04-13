32b89ac8af60390a2f1237612e1d24b07246322a
makutak <takkouno@gmail.com>
2019-04-13 23:37:22 +0900

    add protocol

--- web-server/henacat/src/henacat/servletimpl/http_servlet_response_impl.clj
+++ web-server/henacat/src/henacat/servletimpl/http_servlet_response_impl.clj
@@ -3,4 +3,6 @@
 (def default-content-type "application/octet-stream")
 (def default-character-encoding "UTF-8")
 
+(defprotocol IHttpServletResponseImpl)
+
 (defrecord HttpServletResponseImpl [content-type character-encoding output-stream print-writer])
