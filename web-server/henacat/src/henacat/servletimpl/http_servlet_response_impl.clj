7bda7a8dfd3d53704ada2c7e10dbe3820bcf650e
makutak <takkouno@gmail.com>
2019-04-14 23:40:15 +0900

    メソッド追加

--- web-server/henacat/src/henacat/servletimpl/http_servlet_response_impl.clj
+++ web-server/henacat/src/henacat/servletimpl/http_servlet_response_impl.clj
@@ -3,6 +3,9 @@
 (def default-content-type "application/octet-stream")
 (def default-character-encoding "UTF-8")
 
-(defprotocol IHttpServletResponseImpl)
+(defprotocol IHttpServletResponseImpl
+  (set-content-type [this content-type])
+  (set-character-encoding [this char-set])
+  (get-writer [this]))
 
 (defrecord HttpServletResponseImpl [content-type character-encoding output-stream print-writer])
