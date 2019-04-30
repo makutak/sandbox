1e7d0e0c1c9be087ea4808c12e702ab93f5fc75f
makutak <takkouno@gmail.com>
2019-04-30 21:21:16 +0900

    protocolとrecordの定義

--- web-server/henacat/src/henacat/servletinterfaces/cookie.clj
+++ web-server/henacat/src/henacat/servletinterfaces/cookie.clj
@@ -1,1 +1,18 @@
 (ns henacat.servletinterfaces.cookie)
+
+(defprotocol ICookie
+  (set-domain [this pattern])
+  (get-domain [this])
+  (set-max-age [this expiry])
+  (get-max-age [this])
+  (set-path [this uri])
+  (get-path [this])
+  (set-secure [this flag])
+  (get-secure [this])
+  (set-http-only [this http-only])
+  (is-http-only [this])
+  (get-name [this])
+  (set-value [this new-value])
+  (get-value [this]))
+
+(defrecord Cookie [name value domain max-age path sercure http-only])
