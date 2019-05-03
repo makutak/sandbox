344f513d83c4319565d5f3cc6fdcbe1e4b1533ef
makutak <takkouno@gmail.com>
2019-05-03 22:00:29 +0900

    関数追加

--- web-server/henacat/src/henacat/servletimpl/response_header_generator_impl.clj
+++ web-server/henacat/src/henacat/servletimpl/response_header_generator_impl.clj
@@ -1,6 +1,13 @@
 (ns henacat.servletimpl.response_header_generator_impl
+  (:import [java.text SimpleDateFormat]
+           [java.util Date Calendar TimeZone Locale])
   (:require [henacat.util.response_header_generator :refer [ResponseHeaderGenerator]]))
 
+(defn get-cookie-date-string
+  [cal]
+  (let [df (SimpleDateFormat. "EEE, dd-MMM-yyyy HH:mm:ss" Locale/US)]
+    (.setTimeZone df (.getTimeZone cal))
+    (str (.format df (.getTime cal)) " GMT")))
 
 (defrecord ResponseHeaderGeneratorImpl [cookies]
   ResponseHeaderGenerator
