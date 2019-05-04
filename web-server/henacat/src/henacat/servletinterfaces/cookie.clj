9195972b270455b51f831d22a72fe503a31d777b
makutak <takkouno@gmail.com>
2019-05-04 22:34:08 +0900

    修正

--- web-server/henacat/src/henacat/servletinterfaces/cookie.clj
+++ web-server/henacat/src/henacat/servletinterfaces/cookie.clj
@@ -15,42 +15,42 @@
   (set-value [this new-value])
   (get-value [this]))
 
-(defrecord Cookie [name value domain max-age path sercure http-only]
+(defrecord Cookie [name value domain max-age path secure http-only]
   ICookie
   (set-domain [this pattern]
     (reset! (:domain this) pattern))
   (get-domain [this]
-    (:domain this))
+    @(:domain this))
   (set-max-age [this expiry]
     (reset! (:max-age this) expiry))
   (get-max-age [this]
-    (:max-age this))
+    @(:max-age this))
   (set-path [this uri]
     (reset! (:uri this) uri))
   (get-path [this]
-    (:path this))
+    @(:path this))
   (set-secure [this flag]
-    (reset! (:sercure this) flag))
+    (reset! (:secure this) flag))
   (get-secure [this]
-    (:secure this))
+    @(:secure this))
   (set-http-only [this http-only]
     (reset! (:http-only this) http-only))
   (is-http-only [this]
-    (:http-only this))
+    @(:http-only this))
   (get-name [this]
     (:name this))
   (set-value [this new-value]
     (reset! (:value this) new-value))
   (get-value [this]
-    (:value this)))
+    @(:value this)))
 
 (defn make-Cookie
   [name value]
-  (Cookie. name
-           value
-           (atom nil) ;; domain
-           (atom nil) ;; max-age
-           (atom nil) ;; path
-           (atom nil) ;; secure
-           (atom nil) ;; http-only
+  (Cookie. name         ;; name
+           (atom value) ;; value
+           (atom nil)   ;; domain
+           (atom -1)    ;; max-age
+           (atom nil)   ;; path
+           (atom nil)   ;; secure
+           (atom nil)   ;; http-only
            ))
