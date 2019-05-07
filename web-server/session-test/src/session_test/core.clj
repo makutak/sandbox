e76bc740b0defadbd322271fa0755f522a3f091c
makutak <takkouno@gmail.com>
2019-05-07 22:34:09 +0900

    インクリメントするように

--- web-server/session-test/src/session_test/core.clj
+++ web-server/session-test/src/session_test/core.clj
@@ -13,15 +13,15 @@
    :extends javax.servlet.http.HttpServlet))
 
 (defn -doGet
-  [this ^HttpServletRequest request ^HttpServletResponse response]
-  (println this)
+  [^HttpServlet this ^HttpServletRequest request ^HttpServletResponse response]
   (.setContentType response "text/plain")
   (let [^PrintWriter out (.getWriter response)
         ^HttpSession session (.getSession request true)
-        ^Integer counter (Integer. (.getAttribute session "Counter"))]
+        counter (.getAttribute session "Counter")]
     (if (nil? counter)
       (do
         (.println out "No Session!!")
-        (.setAttribute session "Counter", (Integer. 1)))
+        (.setAttribute session "Counter" 1))
       (do
-        (.println out (str "Counter.. " (inc (Integer. counter))))))))
+        (.println out (str "Counter.. " counter))
+        (.setAttribute session "Counter" (inc (Integer. counter)))))))
