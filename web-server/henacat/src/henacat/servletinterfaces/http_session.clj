b675fe3a7ebae263f5e38e0093d3a9c59778d990
makutak <takkouno@gmail.com>
2019-05-08 22:17:09 +0900

    修正

--- web-server/henacat/src/henacat/servletinterfaces/http_session.clj
+++ web-server/henacat/src/henacat/servletinterfaces/http_session.clj
@@ -2,7 +2,7 @@
 
 (defprotocol HttpSession
   (get-id [this])
-  (get-attribute [this name])
+  (get-attribute [this attribute-name])
   (get-attribute-names [this])
-  (remove-attribute [this name])
-  (set-attribute [this value]))
+  (remove-attribute [this attribute-name])
+  (set-attribute [this attribute-name attribute-value]))
