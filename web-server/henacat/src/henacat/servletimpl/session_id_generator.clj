3bd9bac4c2e4487f8398d5e0068a394efe213d7a
makutak <takkouno@gmail.com>
2019-05-11 21:22:28 +0900

    session_idを生成する

--- web-server/henacat/src/henacat/servletimpl/session_id_generator.clj
+++ web-server/henacat/src/henacat/servletimpl/session_id_generator.clj
@@ -1,14 +1,15 @@
 (ns henacat.servletimpl.session_id_generator
-  (:import [java.security SecureRandom]))
+  (:import [java.security SecureRandom])
+  (:require [clojure.string :as s]))
 
 
-(def generate-session-id
+(defn generate-session-id
   []
-  )
-
-(defn bytes
-  "Returns a random byte array of the specified size."
-  [size]
-  (let [seed (byte-array size)]
-    (.nextBytes (SecureRandom.) seed)
-    seed))
+  (let [random (SecureRandom/getInstance "SHA1PRNG")
+        bytes (byte-array 16)]
+    (.nextBytes random bytes)
+    (s/join ""
+            (map (fn [b] (->> (bit-and b 0xff)
+                              (Integer/toHexString)
+                              (s/upper-case)))
+                 bytes))))
