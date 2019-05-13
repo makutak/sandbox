5f37e4cac11e57b9c527f1fb18cf8f45f24dd41c
makutak <takkouno@gmail.com>
2019-05-13 22:42:47 +0900

    実装中

--- web-server/henacat/src/henacat/servletimpl/session_manager.clj
+++ web-server/henacat/src/henacat/servletimpl/session_manager.clj
@@ -1,9 +1,26 @@
 (ns henacat.servletimpl.session_manager
-  (:import [java.util.concurrent ConcurrentHashMap])
+  (:import [java.util.concurrent ConcurrentHashMap Executors ScheduledExecutorService TimeUnit])
   (:require [henacat.servletimpl.http_session_impl :refer [make-HttpSessionImpl]]
             [henacat.servletimpl.session_id_generator :refer [generate-session-id]]))
+
+(declare clean-sessions)
 
 (def CLEAN_INTERVAL 60)
 (def SESSION_TIMEOUT 10)
 
-(def sessions (ConcurrentHashMap.))
+(def *sessions* (ConcurrentHashMap.))
+(def session-id-generater (generate-session-id))
+(def scheduler
+  (let [scheduler (Executors/newSingleThreadScheduledExecutor)]
+    (.scheduleWithFixedDelay scheduler
+                             #(cleaner-handle) CLEAN_INTERVAL CLEAN_INTERVAL TimeUnit/SECONDS)))
+
+(def cleaner-handle scheduler)
+
+(defn clean-sessions
+  []
+  scheduler)
+
+(defn session-manager
+  []
+  cleaner-handle)
