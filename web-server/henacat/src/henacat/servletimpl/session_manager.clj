8924e34d2eac74f1ce91d0c5a6779adf0a6faba4
makutak <takkouno@gmail.com>
2019-05-13 22:05:34 +0900

    定数定義

--- web-server/henacat/src/henacat/servletimpl/session_manager.clj
+++ web-server/henacat/src/henacat/servletimpl/session_manager.clj
@@ -2,3 +2,8 @@
   (:import [java.util.concurrent ConcurrentHashMap])
   (:require [henacat.servletimpl.http_session_impl :refer [make-HttpSessionImpl]]
             [henacat.servletimpl.session_id_generator :refer [generate-session-id]]))
+
+(def CLEAN_INTERVAL 60)
+(def SESSION_TIMEOUT 10)
+
+(def sessions (ConcurrentHashMap.))
