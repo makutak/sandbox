dde9e767e576642919354425ab9985fd89a8d72d
makutak <takkouno@gmail.com>
2019-05-16 22:46:42 +0900

    各処理でschedulerを呼べば良いのか

--- web-server/henacat/src/henacat/servletimpl/session_manager.clj
+++ web-server/henacat/src/henacat/servletimpl/session_manager.clj
@@ -3,8 +3,7 @@
   (:require [henacat.servletimpl.http_session_impl :refer [make-HttpSessionImpl access]]
             [henacat.servletimpl.session_id_generator :refer [generate-session-id]]))
 
-(declare cleaner-handle
-         clean-sessions)
+(declare clean-sessions)
 
 (def CLEAN_INTERVAL 60)
 (def SESSION_TIMEOUT 10)
@@ -15,15 +14,14 @@
 (def scheduler
   (let [scheduler (Executors/newSingleThreadScheduledExecutor)]
     (.scheduleWithFixedDelay scheduler
-                             #(cleaner-handle)
+                             #(clean-sessions)
                              CLEAN_INTERVAL
                              CLEAN_INTERVAL
                              TimeUnit/SECONDS)))
 
-(def cleaner-handle scheduler)
-
 (defn get-session
   [id]
+  scheduler
   (let [ret (.get *sessions* (keyword id))]
     (when (not (nil? ret))
       (access ret))
@@ -31,6 +29,7 @@
 
 (defn create-session
   []
+  scheduler
   (let [id (generate-session-id)
         session (make-HttpSessionImpl id)]
     (.put *sessions* (keyword id) session)
