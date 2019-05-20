d1d378cac6e2e5a8c8ee850d67eb6e2a609638b8
makutak <takkouno@gmail.com>
2019-05-21 04:26:51 +0900

    修正

--- tutorials/ts-react-tutorial/todo-app/src/reducer.ts
+++ tutorials/ts-react-tutorial/todo-app/src/reducer.ts
@@ -2,13 +2,13 @@ import { combineReducers, Reducer } from 'redux';
 import { TodoActionType, TodoAction, AddTodoAction } from './action';
 import { RootState, TodoState } from './store';
 
-const initTodoState = {
+const initTodoState: TodoState = {
   todos: [],
 };
 
-const todoReducer: Reducer<TodoState> = (
+const todoReducer: Reducer<TodoState, TodoAction> = (
   state: TodoState = initTodoState,
-  action: AddTodoAction,
+  action: TodoAction,
 ): TodoState => {
   switch (action.type) {
     case TodoActionType.ADD_TODO:
