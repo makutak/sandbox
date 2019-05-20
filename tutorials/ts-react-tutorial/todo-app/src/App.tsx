0cace0fcbebc418220708cbbda7bfd0cc9ce2226
makutak <takkouno@gmail.com>
2019-05-21 04:27:07 +0900

    containerを使いたい

--- tutorials/ts-react-tutorial/todo-app/src/App.tsx
+++ tutorials/ts-react-tutorial/todo-app/src/App.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import './App.css';
-import TodoComponent from './TodoComponent';
+import * as React from 'react';
+import { Provider } from 'react-redux';
 
-const App: React.FC = () => {
-  return (
-    <div>
-      <TodoComponent
-        onClickAddButton={(todo: string): void => {
-          console.log(todo);
-        }}
-      />
-    </div>
-  );
+import store from './store';
+import TodoContainer from './TodoContainer';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <Provider store={store}>
+        <TodoContainer />
+      </Provider>
+    );
+  }
 }
-
 export default App;
