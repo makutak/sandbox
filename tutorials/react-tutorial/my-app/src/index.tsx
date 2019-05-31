182be13a98734dc0c34006c4ba184cf3bd533562
makutak <takkouno@gmail.com>
2019-05-31 20:14:12 +0900

    stateを描画するように

--- tutorials/react-tutorial/my-app/src/index.tsx
+++ tutorials/react-tutorial/my-app/src/index.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import './index.css';
 
 interface SquareProps {
-  value: number;
+  value: string;
 }
 
 interface SquareState {
@@ -28,9 +29,22 @@ class Square extends React.Component<SquareProps, SquareState> {
   }
 }
 
-class Board extends React.Component {
+interface BoardProps { }
+interface BoardState {
+  squares: string[],
+}
+
+class Board extends React.Component<BoardProps, BoardState> {
+  constructor(props: BoardProps) {
+    super(props);
+
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
+
   renderSquare(i: number) {
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]} />;
   }
 
   render() {
