7b40bd40b13c515724ea22ac9b34db38b24fcf18
makutak <takkouno@gmail.com>
2019-06-11 22:45:27 +0900

    rotateのactionとreducer

---
+++ tutorials/redux-tutorial/react-redux-tutorial/src/modules/rotate.ts
@@ -0,0 +1,37 @@
+
+export enum actionType {
+  ROTATE = "rotate",
+}
+
+interface RotateAction {
+  type: actionType.ROTATE;
+  payload: boolean;
+}
+
+export const startAction: RotateAction = {
+  type: actionType.ROTATE,
+  payload: true,
+};
+
+export const stopAction: RotateAction = {
+  type: actionType.ROTATE,
+  payload: false,
+};
+
+export type action = RotateAction;
+
+export interface RotateState {
+  rotating: boolean;
+}
+
+export default function rorateReducer(state: RotateState, action: action): RotateState {
+  switch (action.type) {
+    case actionType.ROTATE:
+      return {
+        rotating: action.payload,
+      };
+
+    default:
+      return state;
+  };
+}
