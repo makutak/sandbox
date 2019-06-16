9cad20ec2c42bd51c16d01344b07a8c6c71d8b25
makutak <takkouno@gmail.com>
2019-06-16 23:52:45 +0900

    import actions

--- tutorials/redux-tutorial/react-redux-tutorial/src/App.tsx
+++ tutorials/redux-tutorial/react-redux-tutorial/src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
+import { startAction, stopAction } from './modules/rotate';
 
 import logo from './logo.svg';
 import './App.css';
