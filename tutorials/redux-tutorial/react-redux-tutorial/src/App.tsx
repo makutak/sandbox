7fa63fdf7541e688ed50f94e8afcfcb6f76ea823
makutak <takkouno@gmail.com>
2019-06-17 22:11:33 +0900

    動かない

--- tutorials/redux-tutorial/react-redux-tutorial/src/App.tsx
+++ tutorials/redux-tutorial/react-redux-tutorial/src/App.tsx
@@ -1,29 +1,59 @@
 import * as React from 'react';
+import { Dispatch, Action } from 'redux';
 import { connect } from 'react-redux';
-import { startAction, stopAction } from './modules/rotate';
+import { startAction, stopAction, RotateState } from './modules/rotate';
 
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface DispatchToProps {
+  startAction: () => void;
+  stopAction: () => void;
 }
 
-export default connect()(App);
+type Props = RotateState & DispatchToProps;
+
+class App extends React.Component<Props, {}> {
+  render(): JSX.Element {
+    console.log(this.props);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img
+            src={logo}
+            className={
+              "App-logo" +
+              (this.props.rotating ? "" : " App-logo-paused")
+            }
+            alt="logo"
+            onClick={
+              this.props.rotating ?
+                this.props.stopAction : this.props.startAction
+            } />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+        </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+const mapStateToDispatch = (state: RotateState) => ({
+  ...state,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch<Action>) => ({
+  startAction: () => dispatch(startAction),
+  stopAction: () => dispatch(stopAction),
+});
+
+export default connect(mapStateToDispatch, mapDispatchToProps)(App);
