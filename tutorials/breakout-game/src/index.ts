a2344eed7b0812446b9d2507f57828f1ad164719
makutak <takkouno@gmail.com>
2019-06-21 03:08:10 +0900

    赤い四角を描く

--- tutorials/breakout-game/src/index.ts
+++ tutorials/breakout-game/src/index.ts
@@ -1,2 +1,8 @@
-const foo: string = "foo";
-console.log(foo);
+
+const canvas = <HTMLCanvasElement>document.getElementById('myCanvas');
+const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = '#FF0000';
+ctx.fill();
+ctx.closePath();
