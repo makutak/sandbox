c2376160d2e81aaa8d1eeb3277f7b8ed9ce3ce4e
makutak <takkouno@gmail.com>
2019-08-05 22:20:12 +0900

    shadowingもできる

--- tutorials/rust-tutorial/projects/variables/src/main.rs
+++ tutorials/rust-tutorial/projects/variables/src/main.rs
@@ -7,4 +7,22 @@ fn main() {
 
     const MAX_POINTS: u32 = 100_000;
     println!("The MAX_POINTS is: {}", MAX_POINTS);
+
+    shadowing();
+}
+
+fn shadowing() {
+    println!("##########################");
+    println!("This is shadowing test");
+
+    let x = 5;
+    println!("The value of x is: {}", x);
+
+    let x = 6;
+    println!("The value of x is: {}", x);
+
+    let x = 100_000;
+    println!("The value of x is: {}", x);
+
+    println!("##########################");
 }
