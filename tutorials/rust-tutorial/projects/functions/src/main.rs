d9fd85698c4eedc9f829f01077c14bdea094e8f8
makutak <takkouno@gmail.com>
2019-08-14 02:39:30 +0900

    letを使用して変数を生成し値を代入することは文になる

--- tutorials/rust-tutorial/projects/functions/src/main.rs
+++ tutorials/rust-tutorial/projects/functions/src/main.rs
@@ -2,9 +2,16 @@ fn main() {
     println!("Hello, world!");
 
     another_function(5, 10);
+
+    statement_test();
 }
 
 fn another_function(x: i32, y: i32) {
     println!("The value of x is {}: ", x);
     println!("The value of y is {}: ", y);
+}
+
+fn statement_test() {
+    let a = 5;
+    println!("a: {}: ", a);
 }
