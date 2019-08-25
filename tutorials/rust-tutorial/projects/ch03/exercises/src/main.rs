07336480d74736517ef4c7c041611ae05d98704a
makutak <takkouno@gmail.com>
2019-08-26 01:19:11 +0900

    n番目のフィボナッチ数を求める

--- tutorials/rust-tutorial/projects/ch03/exercises/src/main.rs
+++ tutorials/rust-tutorial/projects/ch03/exercises/src/main.rs
@@ -2,9 +2,24 @@ fn main() {
     assert_eq!(0, fahrenheit_to_centigrade(32));
     assert_eq!(100, fahrenheit_to_centigrade(212));
     assert_eq!(23, fahrenheit_to_centigrade(74));
+
+    assert_eq!(0, fib(0));
+    assert_eq!(1, fib(1));
+    assert_eq!(1, fib(2));
+    assert_eq!(55, fib(10));
 }
 
 //華氏から摂氏への変換
 fn fahrenheit_to_centigrade(val: i32) -> i32 {
     (val - 32) * 5 / 9
+}
+
+fn fib(n: i32) -> i32 {
+    if n == 0 {
+        0
+    } else if n == 1 {
+        1
+    } else {
+        fib(n - 1) + fib(n - 2)
+    }
 }
