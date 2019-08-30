e8f82c22a10f6bbf9a27806166ce7c5d4813f573
makutak <takkouno@gmail.com>
2019-08-31 02:38:42 +0900

    add memo

--- tutorials/rust-tutorial/projects/ch04/src/main.rs
+++ tutorials/rust-tutorial/projects/ch04/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let mut s = String::from("hello");
+    let mut s = String::from("hello"); //ここで必要なメモリが要求される
     assert_eq!("hello", s);
 
-    s.push_str(", world!");
+    s.push_str(", world!"); //メモリを更に要求する
     assert_eq!("hello, world!", s);
 }
