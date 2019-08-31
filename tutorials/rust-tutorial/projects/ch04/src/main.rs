b516fa436912eba931ef6f28c8db28048dc8e5a9
makutak <takkouno@gmail.com>
2019-08-31 21:23:26 +0900

    s1をs2に束縛したらs1は無効になるので、s1は出力できずにコンパイルエラーとなる

--- tutorials/rust-tutorial/projects/ch04/src/main.rs
+++ tutorials/rust-tutorial/projects/ch04/src/main.rs
@@ -4,4 +4,8 @@ fn main() {
 
     s.push_str(", world!"); //メモリを更に要求する
     assert_eq!("hello, world!", s);
+
+    let s1 = String::from("hello");
+    let s2 = s1;
+    println!("{}", s1);
 } //drop関数が呼ばれsのスコープが終わる
