467310d6bc7769afc5a7611a607d24383c3ad7d4
makutak <takkouno@gmail.com>
2019-08-31 21:27:47 +0900

    関数分けた

--- tutorials/rust-tutorial/projects/ch04/src/main.rs
+++ tutorials/rust-tutorial/projects/ch04/src/main.rs
@@ -4,8 +4,11 @@ fn main() {
 
     s.push_str(", world!"); //メモリを更に要求する
     assert_eq!("hello, world!", s);
+    move_test();
+} //drop関数が呼ばれsのスコープが終わる
 
+fn move_test() {
     let s1 = String::from("hello");
     let s2 = s1;
     println!("{}", s2);
-} //drop関数が呼ばれsのスコープが終わる
+}
