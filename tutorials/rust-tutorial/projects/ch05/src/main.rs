e5e2d38702f31a51c8c4c218a5e1e63d1c600815
makutak <takkouno@gmail.com>
2019-09-14 03:25:39 +0900

    mutにすれば値を変更できる

--- tutorials/rust-tutorial/projects/ch05/src/main.rs
+++ tutorials/rust-tutorial/projects/ch05/src/main.rs
@@ -15,4 +15,15 @@ fn main() {
     };
 
     println!("user: {:?}", user);
+
+    let mut user_2 = User {
+        username: String::from("test_2"),
+        email: String::from("test_2@example.com"),
+        sign_in_count: 2,
+        active: true,
+    };
+    assert_eq!(user_2.email, "test_2@example.com");
+
+    user_2.email = String::from("updated@example.com");
+    assert_eq!(user_2.email, "updated@example.com");
 }
