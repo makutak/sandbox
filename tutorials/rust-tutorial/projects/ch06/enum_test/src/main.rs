de52726380251c218654e6e45f1d3c32a9a17e91
makutak <takkouno@gmail.com>
2019-10-13 02:56:31 +0900

    if let else を使ってこう書くこともできる

--- tutorials/rust-tutorial/projects/ch06/enum_test/src/main.rs
+++ tutorials/rust-tutorial/projects/ch06/enum_test/src/main.rs
@@ -134,9 +134,10 @@ fn main() {
 
     let coin = Coin::Penny;
     let mut count = 0;
-    match coin {
-        Coin::Quarter(state) => println!("State quarter from {:?}!", state),
-        _ => count += 1,
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
     }
     println!("count is: {}", count);
 }
