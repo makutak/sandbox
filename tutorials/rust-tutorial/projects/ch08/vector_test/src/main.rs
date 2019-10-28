4b2d32a50e52c88673f407c4d61823e57ea36beb
makutak <takkouno@gmail.com>
2019-10-28 21:23:06 +0900

    pushメソッドを使ってベクタ型に値を追加できる

--- tutorials/rust-tutorial/projects/ch08/vector_test/src/main.rs
+++ tutorials/rust-tutorial/projects/ch08/vector_test/src/main.rs
@@ -1,5 +1,13 @@
 fn main() {
     let v = vec![1, 2, 3];
-
     println!("{:?}", v);
+
+    let mut v2: Vec<i32> = Vec::new();
+    println!("{:?}", v2);
+
+    v2.push(5);
+    v2.push(6);
+    v2.push(7);
+    v2.push(8);
+    println!("{:?}", v2);
 }
