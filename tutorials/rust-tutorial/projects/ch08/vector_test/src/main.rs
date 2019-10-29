796368007f78a59177c4ca7bfcd719002252dd94
makutak <takkouno@gmail.com>
2019-10-29 22:20:26 +0900

    借用規則によりエラーになる

--- tutorials/rust-tutorial/projects/ch08/vector_test/src/main.rs
+++ tutorials/rust-tutorial/projects/ch08/vector_test/src/main.rs
@@ -2,27 +2,31 @@ fn main() {
     let v = vec![1, 2, 3];
     println!("{:?}", v);
 
-    let mut v2: Vec<i32> = Vec::new();
-    println!("{:?}", v2);
+    let mut v: Vec<i32> = Vec::new();
+    println!("{:?}", v);
 
-    v2.push(5);
-    v2.push(6);
-    v2.push(7);
-    v2.push(8);
-    println!("{:?}", v2);
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+    println!("{:?}", v);
 
-    let v3 = vec![1, 2, 3, 4, 5];
+    let v = vec![1, 2, 3, 4, 5];
 
-    let third: &i32 = &v3[2];
+    let third: &i32 = &v[2];
     println!("{}", third);
     let third: Option<&i32> = v.get(2);
     println!("{:?}", third);
 
-    let v4 = vec![1, 2, 3, 4, 5];
+    let v = vec![1, 2, 3, 4, 5];
 
     // let does_not_exist = &v4[100];
     // println!("{:?}", does_not_exist);
 
-    let does_not_exist = v4.get(100);
+    let does_not_exist = v.get(100);
     println!("{:?}", does_not_exist);
+
+    let v = vec![1, 2, 3, 4, 5];
+    let first = &v[0];
+    v.push(6);
 }
