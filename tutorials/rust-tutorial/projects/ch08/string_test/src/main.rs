513b5d11974bd1153267e2e4ae2ca4b0adf6e4b9
makutak <takkouno@gmail.com>
2019-11-01 21:48:31 +0900

    文字列リテラルからstringを生成する

--- tutorials/rust-tutorial/projects/ch08/string_test/src/main.rs
+++ tutorials/rust-tutorial/projects/ch08/string_test/src/main.rs
@@ -1,4 +1,13 @@
 fn main() {
     let mut s = String::new();
     println!("{}", s);
+
+    let data = "initial contents";
+    println!("data: {}", data);
+
+    let s = data.to_string();
+    println!("s: {}", s);
+
+    let s = "initial contents".to_string();
+    println!("s: {}", s);
 }
