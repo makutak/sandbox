b1477f63ab778f4ef9899397d969bfa563a04ae2
makutak <takkouno@gmail.com>
2019-11-02 21:34:08 +0900

    push_strメソッドは引数の所有権を得なくても良い

--- tutorials/rust-tutorial/projects/ch08/string_test/src/main.rs
+++ tutorials/rust-tutorial/projects/ch08/string_test/src/main.rs
@@ -19,4 +19,9 @@ fn main() {
 
     s.push_str(" bar");
     println!("s: {}", s);
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is {}", s2);
 }
