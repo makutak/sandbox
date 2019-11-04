541c0f6aff7544372294a491db0f569b3620ac82
makutak <takkouno@gmail.com>
2019-11-04 20:01:40 +0900

    複数の文字列を連結するときはformat!マクロを使うほうが良い

--- tutorials/rust-tutorial/projects/ch08/string_test/src/main.rs
+++ tutorials/rust-tutorial/projects/ch08/string_test/src/main.rs
@@ -33,4 +33,10 @@ fn main() {
     let s2 = String::from("world!!");
     let s3 = s1 + &s2;
     println!("s3 is {}", s3);
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    let s = format!("{}-{}-{}", s1, s2, s3);
+    println!("s is {}", s);
 }
