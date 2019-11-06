cd5b78c5c147420cfb0468ab0ad2574991bc79a9
makutak <takkouno@gmail.com>
2019-11-06 22:16:18 +0900

    チームのリストとスコアのリストからハッシュマップを作る

--- tutorials/rust-tutorial/projects/ch08/hashmap_test/src/main.rs
+++ tutorials/rust-tutorial/projects/ch08/hashmap_test/src/main.rs
@@ -5,6 +5,11 @@ fn main() {
 
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 50);
+    println!("scores is {:?}", scores);
 
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
     println!("scores is {:?}", scores);
 }
