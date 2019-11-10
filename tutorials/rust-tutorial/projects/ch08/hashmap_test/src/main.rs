c6860565116b990987cf049dae947566b00c0e73
makutak <takkouno@gmail.com>
2019-11-10 20:30:56 +0900

    単語とカウントを保持するハッシュマップを使って単語の出現数をカウントする

--- tutorials/rust-tutorial/projects/ch08/hashmap_test/src/main.rs
+++ tutorials/rust-tutorial/projects/ch08/hashmap_test/src/main.rs
@@ -49,4 +49,14 @@ fn main() {
     scores.entry(String::from("Yellow")).or_insert(50);
     scores.entry(String::from("Blue")).or_insert(50);
     println!("scores: {:?}", scores);
+
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        println!("count: {}", count);
+        *count += 1;
+    }
+    println!("map: {:?}", map);
 }
