aa0825c45f443ceda3440afa19dcd87b0076070a
makutak <takkouno@gmail.com>
2019-11-16 21:35:06 +0900

    match式を使用して返却される可能性のあるResult列挙子を処理する

--- tutorials/rust-tutorial/projects/ch09/result_test/src/main.rs
+++ tutorials/rust-tutorial/projects/ch09/result_test/src/main.rs
@@ -2,4 +2,9 @@ use std::fs::File;
 
 fn main() {
     let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => panic!("There was a problem opening the file: {:?}", error),
+    };
 }
