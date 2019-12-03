a47507b81bdf853f847792a25abe18bb97b13358
makutak <takkouno@gmail.com>
2019-12-04 03:43:31 +0900

    スコープを変えても通る

--- tutorials/rust-tutorial/projects/ch10/generics/src/main.rs
+++ tutorials/rust-tutorial/projects/ch10/generics/src/main.rs
@@ -36,8 +36,10 @@ fn main() {
     }
 
     let string1 = String::from("abcd");
-    let string2 = "xyz";
+    {
+        let string2 = "xyz";
 
-    let result = longest(string1.as_str(), string2);
-    println!("The longest string is {}", result);
+        let result = longest(string1.as_str(), string2);
+        println!("The longest string is {}", result);
+    }
 }
