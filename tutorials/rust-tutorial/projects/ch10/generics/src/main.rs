a0f17f5f80c8e0eb34e929dececbf8c938497921
makutak <takkouno@gmail.com>
2019-12-05 02:31:59 +0900

    スコープが違うのでコンパイルできない

--- tutorials/rust-tutorial/projects/ch10/generics/src/main.rs
+++ tutorials/rust-tutorial/projects/ch10/generics/src/main.rs
@@ -35,11 +35,12 @@ fn main() {
         println!("r: {}", r);
     }
 
-    let string1 = String::from("abcd");
+    let string1 = String::from("long string is long");
+    let result;
     {
-        let string2 = "xyz";
+        let string2 = String::from("xyz");
 
-        let result = longest(string1.as_str(), string2);
-        println!("The longest string is {}", result);
+        result = longest(string1.as_str(), string2.as_str());
     }
+    println!("The longest string is {}", result);
 }
