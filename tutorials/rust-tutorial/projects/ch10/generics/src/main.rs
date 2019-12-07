0919389bdbdb419c471ec4ad6595f9f3428476c0
makutak <takkouno@gmail.com>
2019-12-07 21:29:01 +0900

    修正

--- tutorials/rust-tutorial/projects/ch10/generics/src/main.rs
+++ tutorials/rust-tutorial/projects/ch10/generics/src/main.rs
@@ -37,9 +37,7 @@ fn main() {
 
     let string1 = String::from("long string is long");
     let result;
-    {
-        let string2 = String::from("xyz");
-        result = longest(string1.as_str(), string2.as_str());
-        println!("The longest string is {}", result);
-    }
+    let string2 = String::from("xyz");
+    result = longest(string1.as_str(), string2.as_str());
+    println!("The longest string is {}", result);
 }
