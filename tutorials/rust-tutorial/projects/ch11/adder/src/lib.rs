cb0eb225203bf745e9c831e12cebdcb91cd76464
makutak <takkouno@gmail.com>
2019-12-11 23:56:43 +0900

    rectangle構造体とそのcan_holdメソッドのテストを追加

--- tutorials/rust-tutorial/projects/ch11/adder/src/lib.rs
+++ tutorials/rust-tutorial/projects/ch11/adder/src/lib.rs
@@ -1,12 +1,36 @@
+#[derive(Debug)]
+pub struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    pub fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+impl Rectangle {
+    pub fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn explanation() {
-        assert_eq!(2 + 2, 4);
-    }
+    use super::*;
 
     #[test]
-    fn another() {
-        panic!("Make this teset fail!!");
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            height: 8,
+            width: 7,
+        };
+        let smaller = Rectangle {
+            height: 5,
+            width: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
     }
 }
