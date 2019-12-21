2c99935fdbf028dcd723b20efd502776288cc6ff
makutak <takkouno@gmail.com>
2019-12-21 19:49:17 +0900

    テスト修正

--- tutorials/rust-tutorial/projects/ch11/adder/src/lib.rs
+++ tutorials/rust-tutorial/projects/ch11/adder/src/lib.rs
@@ -30,7 +30,7 @@ pub struct Guess {
 
 impl Guess {
     pub fn new(value: u32) -> Guess {
-        if value < 1 {
+        if value < 1 || value > 100 {
             panic!("Guess value must be between 1 and 100, got {}.", value);
         }
 
