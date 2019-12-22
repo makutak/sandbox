93b95a46982cad2c13769311c2c9de5904359c62
makutak <takkouno@gmail.com>
2019-12-23 01:44:25 +0900

    値の大小によって表示するメッセージを変える

--- tutorials/rust-tutorial/projects/ch11/adder/src/lib.rs
+++ tutorials/rust-tutorial/projects/ch11/adder/src/lib.rs
@@ -32,6 +32,11 @@ impl Guess {
     pub fn new(value: u32) -> Guess {
         if value < 1 {
             panic!("Guess value must be between 1 and 100, got {}.", value);
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
         }
 
         Guess { value }
@@ -86,7 +91,7 @@ mod tests {
     }
 
     #[test]
-    #[should_panic]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
     fn greater_than_100() {
         Guess::new(200);
     }
