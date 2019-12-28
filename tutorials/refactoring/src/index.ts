c779e768eb48bc741bca2cb886a7cb1a2e01181c
makutak <takkouno@gmail.com>
2019-12-29 00:38:05 +0900

    インターフェースも扱える

--- tutorials/refactoring/src/index.ts
+++ tutorials/refactoring/src/index.ts
@@ -1,7 +1,15 @@
 "use strict";
 
+interface Rectangle {
+  height: number;
+  width: number;
+}
+
 const sum: number = 1 + 1;
 console.log("sum:", sum);
 
 const str: string = "Hello" + ", " + "Docker" + "!!";
 console.log("str: ", str);
+
+const rect: Rectangle = { height: 50, width: 50 };
+console.dir(rect);
