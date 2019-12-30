9f42e9f639e8b5f92ced1b7418235ced9fad424e
makutak <takkouno@gmail.com>
2019-12-30 19:14:42 +0900

    BMIを計算できる

--- tutorials/refactoring/src/index.ts
+++ tutorials/refactoring/src/index.ts
@@ -5,6 +5,21 @@ interface Rectangle {
   width: number;
 }
 
+class Human {
+  height: number;
+  weight: number;
+
+  constructor(height: number, weight: number) {
+    this.height = height;
+    this.weight = weight;
+  }
+
+  public calcBmi(): number {
+    const h = this.height / 100;
+    return (this.weight / (h * h));
+  }
+}
+
 const sum: number = 1 + 1;
 console.log("sum:", sum);
 
@@ -13,3 +28,6 @@ console.log("str: ", str);
 
 const rect: Rectangle = { height: 50, width: 50 };
 console.dir(rect);
+
+const human = new Human(170, 55);
+console.log("your BMI is: ", human.calcBmi());
