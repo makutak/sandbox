f18552d066a0e3160075311f53e0bf788d1f6424
makutak <takkouno@gmail.com>
2020-01-05 22:25:23 +0900

    statement関数を実行できるように

--- tutorials/refactoring/src/index.ts
+++ tutorials/refactoring/src/index.ts
@@ -2,6 +2,6 @@ import { statement } from "./invoice";
 const plays = require("./playes.json");
 const invoices = require("./invoices.json");
 
-
 console.log(JSON.stringify(plays, null, "  "));
 console.log(JSON.stringify(invoices, null, "  "));
+statement(invoices, plays);
