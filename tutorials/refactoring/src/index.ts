f47d34eeb473498207354e3c69e8a062c54c54f2
makutak <takkouno@gmail.com>
2020-01-08 00:17:34 +0900

    配列を走査するように

--- tutorials/refactoring/src/index.ts
+++ tutorials/refactoring/src/index.ts
@@ -4,5 +4,7 @@ const invoices = require("./invoices.json");
 
 console.log(JSON.stringify(plays, null, "  "));
 console.log(JSON.stringify(invoices, null, "  "));
-const ret = statement(invoices[0], plays);
-console.log(ret);
+invoices.forEach((invoice: any) => {
+  const ret = statement(invoice, plays);
+  console.log(ret);
+});
