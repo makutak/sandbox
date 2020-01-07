504122582ea6b76c773c1708196c2b88981a5511
makutak <takkouno@gmail.com>
2020-01-07 21:41:31 +0900

    関数分けた

--- tutorials/refactoring/src/invoice.ts
+++ tutorials/refactoring/src/invoice.ts
@@ -30,25 +30,7 @@ export function statement(invoice: Invoice, plays: Plays): string {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
-
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
+    let thisAmount: number = amountFor(perf, play);
 
     volumeCredits += Math.max(perf.audience - 30, 0);
 
@@ -61,4 +43,28 @@ export function statement(invoice: Invoice, plays: Plays): string {
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
+}
+
+export function amountFor(perf: Performance, play: Play): number {
+  let thisAmount: number = 0;
+
+  switch (play.type) {
+    case "tragedy":
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy":
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+  }
+
+  return thisAmount;
 }
