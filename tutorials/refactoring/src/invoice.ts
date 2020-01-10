76c3d3d3bfe4a670fb99665ab06f2aecf4d9373e
makutak <takkouno@gmail.com>
2020-01-10 22:54:01 +0900

    引数名変更

--- tutorials/refactoring/src/invoice.ts
+++ tutorials/refactoring/src/invoice.ts
@@ -49,22 +49,22 @@ export function statement(invoice: Invoice, plays: Plays): string {
   return result;
 }
 
-export function amountFor(perf: Performance, play: Play): number {
+export function amountFor(aPerformance: Performance, play: Play): number {
   let result: number = 0;
 
   switch (play.type) {
     case "tragedy":
       result = 40000;
-      if (perf.audience > 30) {
-        result += 1000 * (perf.audience - 30);
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
     case "comedy":
       result = 30000;
-      if (perf.audience > 20) {
-        result += 10000 + 500 * (perf.audience - 20);
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      result += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
     default:
       throw new Error(`unknown type: ${play.type}`);
