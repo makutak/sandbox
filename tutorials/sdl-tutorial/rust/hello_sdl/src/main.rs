edf44bfd779dea5e3dee59fc8dbd1d382b9f3662
makutak <takkouno@gmail.com>
2020-01-20 22:48:39 +0900

    わからん

--- tutorials/sdl-tutorial/rust/hello_sdl/src/main.rs
+++ tutorials/sdl-tutorial/rust/hello_sdl/src/main.rs
@@ -1,5 +1,7 @@
 extern crate sdl2;
 
+use sdl2::pixels::Color;
+
 fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
@@ -7,9 +9,12 @@ fn main() {
     let window = video_subsystem
         .window("hello", 800, 600)
         .position_centered()
-        .opengl()
         .build()
         .unwrap();
 
     let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
 }
