2b70e1d62ed5746f4a68ade7b06e2000d9b4f711
makutak <takkouno@gmail.com>
2020-02-04 01:32:53 +0900

    array_stackを実装したい

--- ods/rust/ods/src/main.rs
+++ ods/rust/ods/src/main.rs
@@ -1,9 +1,18 @@
-pub struct Array<T> {
-    a: T,
-    length: u32,
+#[derive(Debug)]
+struct Array {
+    a: Vec<u32>,
+    n: u32,
+}
+
+impl Array {
+    pub fn new() -> Array {
+        let mut vec = Vec::new();
+        Array { a: vec, n: 0 }
+    }
 }
 
 fn main() {
     println!("Hello, world!");
-    let ary = Array { a: 1, length: 2 };
+    let ary = Array::new();
+    println!("Array: {:?}", ary);
 }
