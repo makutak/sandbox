21ea94e18a312fdcc23d4f72b9fe6a162a992ae1
makutak <takkouno@gmail.com>
2020-02-05 21:31:58 +0900

    array_stack module を使えるようにした

--- ods/rust/ods/src/array_stack.rs
+++ ods/rust/ods/src/array_stack.rs
@@ -1,17 +1,15 @@
-mod array_stack {
-    struct Array {
-        a: Vec<u32>,
-        n: u32,
+pub struct Array {
+    a: Vec<u32>,
+    n: u32,
+}
+
+impl Array {
+    pub fn new() -> Array {
+        let mut vec = Vec::new();
+        Array { a: vec, n: 0 }
     }
 
-    impl Array {
-        fn new() -> Array {
-            let mut vec = Vec::new();
-            Array { a: vec, n: 0 }
-        }
-
-        fn size(&self) -> u32 {
-            self.n
-        }
+    pub fn size(&self) -> u32 {
+        self.n
     }
 }
--- ods/rust/ods/src/lib.rs
+++ ods/rust/ods/src/lib.rs
@@ -3,9 +3,11 @@ pub mod array_stack;
 #[cfg(test)]
 mod tests {
     use super::*;
+    use array_stack::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(2, 2);
+    fn new_array_stack_size_is_zero() {
+        let ary = Array::new();
+        assert_eq!(ary.size(), 0);
     }
 }
