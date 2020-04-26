6d86116f9335f87cfece4d4a9c44816402f75e0d
makutak <takkouno@gmail.com>
2020-04-26 20:38:46 +0900

    レンダラーのサイズ指定

--- tutorials/three-js-intro/src/index.js
+++ tutorials/three-js-intro/src/index.js
@@ -2,9 +2,13 @@ import * as THREE from "three";
 
 window.addEventListener("DOMContentLoaded", init);
 
+const WIDTH = 960;
+const HEIGHT = 540;
+
 function init() {
   const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector("#my-canvas"),
   });
   console.log("renderer: ", renderer);
+  renderer.setSize(WIDTH, HEIGHT);
 }
