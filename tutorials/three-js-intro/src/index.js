78b4941b2b242a4fdb098b63f951c48f3bb28a1d
makutak <takkouno@gmail.com>
2020-04-27 01:29:32 +0900

    立方体を作成する

--- tutorials/three-js-intro/src/index.js
+++ tutorials/three-js-intro/src/index.js
@@ -18,4 +18,22 @@ function init() {
   // new THREE.PerspectiveCamera(画角, アスペクト比, 描画開始距離, 描画終了距離)
   const camera = new THREE.PerspectiveCamera(45, 800 / 600, 1, 10000);
   console.log("camera: ", camera);
+
+
+  /**
+   * 立方体は「メッシュ」という表示オブジェクトを使用して作成する。
+   * メッシュを作るにはジオメトリ(形状)とマテリアル(素材)を用意する必要がある。
+   */
+
+  // new THREE.BoxGeometry(幅, 高さ, 奥行き)
+  // - ジオメトリは頂点情報や面情報を持ってる。
+  const geometry = new THREE.BoxGeometry(500, 500, 500);
+
+  // - マテリアルは色や質感の情報を持ってる。
+  const material = new THREE.MeshStandardMaterial({ color: 0x0000ff });
+
+  // new THREE.Mesh(ジオメトリ,マテリアル)
+  const mesh = new THREE.Mesh(geometry, material);
+  // シーンに追加
+  scene.add(mesh);
 }
