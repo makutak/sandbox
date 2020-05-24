41895927af75671f38194d513044efc1e81b58d9
makutak <takkouno@gmail.com>
2020-05-24 21:41:43 +0900

    Added deps

--- tutorials/clojurescript-tdd-application/project.clj
+++ tutorials/clojurescript-tdd-application/project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.764"]]
+  :plugins [[lein-cljsbuild "1.1.8" :exclusions [[org.clojure/clojure]]]
+            [lein-figwheel "0.5.18"]]
   :main ^:skip-aot clojurescript-tdd-application.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
