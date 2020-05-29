53cfad7d4d47031da1e542a6ab8c2e0fef3b8658
makutak <takkouno@gmail.com>
2020-05-30 01:46:58 +0900

    Added test files

---
+++ tutorials/clojurescript-tdd-application/test/runners/tests.cljs
@@ -0,0 +1,7 @@
+(ns runners.tests
+  ;; (:require [space-invaders.game-test]
+  ;;           [space-invaders.view-test]
+  ;;           [util.game-loop-test]
+  ;;           [util.image-loader-test])
+  )
+
