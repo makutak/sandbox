abb312dc39dc2f0de4b39f78926089bcc95dc5ab
makutak <takkouno@gmail.com>
2020-06-01 02:50:26 +0900

    Added devcards config

--- tutorials/clojurescript-tdd-application/project.clj
+++ tutorials/clojurescript-tdd-application/project.clj
@@ -3,10 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[devcards "0.2.1-7"]
+                 [lein-doo "0.1.7"]
+                 [org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.764"]]
   :plugins [[lein-cljsbuild "1.1.8" :exclusions [[org.clojure/clojure]]]
-            [lein-doo "0.1.10"]
+            [lein-doo "0.1.7"]
             [lein-figwheel "0.5.20"]]
   :clean-targets ^{:protect false} [:target-path "out" "resources/public/cljs"]
   :figwheel {:css-dirs ["resources/public/css"]}
@@ -25,4 +27,13 @@
                         :source-paths ["src" "test"]
                         :compiler {:main runners.doo
                                    :optimizations :none
-                                   :output-to "resources/public/cljs/tests/all-tests.js"}}]})
+                                   :output-to "resources/public/cljs/tests/all-tests.js"}}
+                       {:id "devcards-test"
+                        :source-paths ["src" "test"]
+                        :figwheel {:devcards true}
+                        :compiler {:main runners.browser
+                                   :optimizations :none
+                                   :asset-path "cljs/tests/out"
+                                   :output-dir "resources/public/cljs/tests/out"
+                                   :output-to "resources/public/cljs/tests/all-tests.js"
+                                   :source-map-timestamp true}}]})
