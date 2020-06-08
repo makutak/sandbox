e2814862b20ec726476f140a07a9b0cf556aa71e
makutak <takkouno@gmail.com>
2020-06-08 23:48:05 +0900

    ひとまず定義

--- tutorials/clj-three/src/clj_three/core.cljs
+++ tutorials/clj-three/src/clj_three/core.cljs
@@ -9,6 +9,22 @@
 
 (defn init []
   (let [scene (js/THREE.Scene.)
+        fog (-> scene (.-fog) (js/THREE.FogExp2. 0xcccccc 0015))
+        renderer (js/THREE.WebGLRenderer.)
+        container (-> js/document (.getElementById "app"))
+        camera (js/THREE.PerspectiveCamera.
+                60
+                (/ (.-innerWidth js/window) (.-innerHeight js/window))
+                1
+                2000)]
+    (*print-fn* fog)
+    (aset camera "position" "y" 20)
+    (aset camera "position" "x" 0)
+    (aset camera "position" "z" 0)
+    ))
+
+(defn old-init []
+  (let [scene (js/THREE.Scene.)
         p-camera (js/THREE.PerspectiveCamera. view-angle aspect near far)
         box (js/THREE.BoxGeometry. 200 200 200)
         mat (js/THREE.MeshBasicMaterial.
