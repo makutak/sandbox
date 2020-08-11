87c3720833912254dd0ec13a04bfb79e46e13ce2
makutak <takkouno@gmail.com>
2020-08-12 03:05:16 +0900

    GETリクエストできるように

--- labo/networks/http-client/http_client.rs
+++ labo/networks/http-client/http_client.rs
@@ -1,17 +1,26 @@
-use std::net::TcpStream;
+use std::io::Write;
+use std::net::{Shutdown, TcpStream};
 
 fn send_msg() {}
 
 fn recv_msg() {}
 
 fn main() {
-    let stream = TcpStream::connect("127.0.0.1:80");
-    match stream {
+    let client_socket = TcpStream::connect("127.0.0.1:80");
+    match client_socket {
         Err(_) => {
-            println!("connection error!");
+            println!("could not connect!!!");
         }
-        Ok(stream) => {
-            println!("connect!!");
+        Ok(mut stream) => {
+            println!("connect!!!");
+            let request_text = "GET / HTTP/1.0\r\n\r\n";
+            stream
+                .write(request_text.as_bytes())
+                .expect("failed to write!!");
+
+            stream
+                .shutdown(Shutdown::Both)
+                .expect("shutdown call failed");
         }
     }
 }
