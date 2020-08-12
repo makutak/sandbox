61535a7755b8b1ebf57ad7114b611d2653393d64
makutak <takkouno@gmail.com>
2020-08-13 03:51:10 +0900

    responseを表示できる

--- labo/networks/http-client/http_client.rs
+++ labo/networks/http-client/http_client.rs
@@ -1,9 +1,17 @@
-use std::io::Write;
+use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::net::{Shutdown, TcpStream};
+use std::str;
 
-fn send_msg() {}
+fn recv_msg(reader: &mut BufReader<&TcpStream>) {
+    for line in reader.lines() {
+        println!("{}", line.unwrap());
+    }
+}
 
-fn recv_msg() {}
+fn send_msg(writer: &mut BufWriter<&TcpStream>, msg: &str) {
+    writer.write(msg.as_bytes()).expect("SEND FAILURE!!!");
+    writer.flush().unwrap();
+}
 
 fn main() {
     let client_socket = TcpStream::connect("127.0.0.1:80");
@@ -11,12 +19,12 @@ fn main() {
         Err(_) => {
             println!("could not connect!!!");
         }
-        Ok(mut stream) => {
-            println!("connect!!!");
+        Ok(stream) => {
             let request_text = "GET / HTTP/1.0\r\n\r\n";
-            stream
-                .write(request_text.as_bytes())
-                .expect("failed to write!!");
+            let mut writer = BufWriter::new(&stream);
+            let mut reader = BufReader::new(&stream);
+            send_msg(&mut writer, request_text);
+            recv_msg(&mut reader);
 
             stream
                 .shutdown(Shutdown::Both)
