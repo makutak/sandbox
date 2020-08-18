82b1ab56400d442511ee9d8b05abfa5967b957a2
makutak <takkouno@gmail.com>
2020-08-19 03:52:39 +0900

    ひとまずマルチスレッドで接続を待ち受ける

--- labo/networks/echo-server/echo_server.rs
+++ labo/networks/echo-server/echo_server.rs
@@ -1,11 +1,18 @@
 use std::io::Error;
 use std::net::TcpListener;
+use std::thread;
 
 fn main() -> Result<(), Error> {
-    let server_socket = TcpListener::bind("127.0.0.1:80");
-
-    match server_socket {
-        Ok(v) => println!("{:?}", v),
+    let listners = TcpListener::bind("127.0.0.1:54321");
+    match listners {
+        Ok(listner) => {
+            for stream in listner.incoming() {
+                thread::spawn(move || {
+                    println!("{:?}", stream);
+                });
+            }
+            drop(listner);
+        }
         Err(e) => panic!("Error!!!!!!!!!!!!!: {:?}", e),
     }
 
