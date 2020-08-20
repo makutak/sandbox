2545f95cec2c01f60a3665363b1a08c87aa57d44
makutak <takkouno@gmail.com>
2020-08-21 02:59:59 +0900

    値を読み込んで出力するだけ

--- labo/networks/echo-server/echo_server.rs
+++ labo/networks/echo-server/echo_server.rs
@@ -1,20 +1,41 @@
-use std::io::Error;
-use std::net::TcpListener;
+use std::io::{BufRead, BufReader};
+use std::net::{TcpListener, TcpStream};
 use std::thread;
 
-fn main() -> Result<(), Error> {
-    let listners = TcpListener::bind("127.0.0.1:54321")?;
-    println!("starting server...");
-    for streams in listners.incoming() {
-        match streams {
-            Ok(stream) => {
-                thread::spawn(move || {
-                    let client = stream.peer_addr().unwrap();
-                    println!("accepcted from: {}:{}", client.ip(), client.port());
-                });
+fn recv(reader: &mut BufReader<&TcpStream>) {
+    for line in reader.lines() {
+        let input = match line {
+            Ok(line) => line,
+            Err(e) => String::from(format!("read error: {:?}", e)),
+        };
+
+        println!("input: {:?}", input);
+    }
+}
+
+fn main() -> Result<(), String> {
+    let listner = TcpListener::bind("127.0.0.1:54321");
+
+    match listner {
+        Ok(listner) => {
+            println!("starting server...");
+
+            for stream in listner.incoming() {
+                match stream {
+                    Ok(stream) => {
+                        thread::spawn(move || {
+                            let client = stream.peer_addr().unwrap();
+                            println!("accepcted from: {}:{}", client.ip(), client.port());
+                            let mut reader = BufReader::new(&stream);
+                            recv(&mut reader);
+                        });
+                    }
+                    Err(e) => println!("accept failed!!!: {:?}", e),
+                }
             }
-            Err(e) => panic!("{:?}", e),
+            drop(listner);
         }
+        Err(e) => println!("server start failed!!!!!!!!!!!!!: {:?}", e),
     }
     Ok(())
 }
