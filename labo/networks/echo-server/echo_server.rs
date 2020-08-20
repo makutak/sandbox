fcd70776f699bec6b9dbb0fe49badd9458ba3da9
makutak <takkouno@gmail.com>
2020-08-21 02:05:52 +0900

    冗長だったので

--- labo/networks/echo-server/echo_server.rs
+++ labo/networks/echo-server/echo_server.rs
@@ -3,25 +3,18 @@ use std::net::TcpListener;
 use std::thread;
 
 fn main() -> Result<(), Error> {
-    let listners = TcpListener::bind("127.0.0.1:54321");
-    match listners {
-        Ok(listner) => {
-            println!("starting server...");
-            for stream in listner.incoming() {
-                match stream {
-                    Ok(stream) => {
-                        thread::spawn(move || {
-                            let client = stream.peer_addr().unwrap();
-                            println!("accepcted from: {}:{}", client.ip(), client.port());
-                        });
-                    }
-                    Err(e) => panic!("{:?}", e),
-                }
+    let listners = TcpListener::bind("127.0.0.1:54321")?;
+    println!("starting server...");
+    for streams in listners.incoming() {
+        match streams {
+            Ok(stream) => {
+                thread::spawn(move || {
+                    let client = stream.peer_addr().unwrap();
+                    println!("accepcted from: {}:{}", client.ip(), client.port());
+                });
             }
-            drop(listner);
+            Err(e) => panic!("{:?}", e),
         }
-        Err(e) => panic!("Error!!!!!!!!!!!!!: {:?}", e),
     }
-
     Ok(())
 }
