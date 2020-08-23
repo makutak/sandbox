e061c497d419b842954a8b293f63ffc45e2dddb6
makutak <takkouno@gmail.com>
2020-08-24 04:32:03 +0900

    ソケットから特定のバイト数を読み込む

--- labo/networks/add-protocol/add_server.rs
+++ labo/networks/add-protocol/add_server.rs
@@ -1,10 +1,31 @@
+use std::io::Read;
 use std::net::{TcpListener, TcpStream};
 
-fn recv_msg(sock: &TcpStream, total_msg_size: i64) {
+fn recv_msg(mut sock: &TcpStream, total_msg_size: usize) -> Vec<u8> {
     // これまでに受信できたバイト数
     let mut total_recv_size = 0;
 
-    //while total_recv_size < total_msg_size {}
+    let result = loop {
+        let buf_size = total_msg_size - total_recv_size;
+        let mut buf = vec![0; buf_size];
+        //let received_chunk = sock.take(buf_size as u64).read_to_end(&mut buf);
+        let received_chunk = sock.read_exact(&mut buf);
+        println!("received_chunk: {:?}", received_chunk);
+        println!("buf: {:?}", buf);
+
+        if total_recv_size < total_msg_size {
+            break buf;
+        }
+
+        if buf.len() == 0 {
+            // 1バイトも読めなかったときはソケットの接続が終了している
+            panic!("socket connection broken");
+        }
+
+        total_recv_size += buf.len();
+    };
+
+    result
 }
 
 fn main() {
@@ -21,4 +42,6 @@ fn main() {
     };
 
     println!("accepted from: {}", addr);
+    let received_msg = recv_msg(&client_socket, 8);
+    println!("received_msg: {:?}", received_msg);
 }
