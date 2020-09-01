25916c28756c1a0deeb2a0f5613d8897357a5d8d
makutak <takkouno@gmail.com>
2020-09-02 04:11:52 +0900

    参照外し

---
+++ tutorials/tour_of_rust/ch05/50.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut foo = 42;
+    let f = &mut foo;
+    println!("f: {}", f);
+    let bar = *f; // 所有者の値を首都高
+    *f = 13; // 参照の所有者の値を設定
+    println!("f: {}", f);
+    println!("bar: {}", bar);
+    println!("foo: {}", foo);
+}
