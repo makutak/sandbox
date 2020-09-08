4a1aeb2f7067a421af9a932df28db9041e3c3591
makutak <takkouno@gmail.com>
2020-09-09 03:32:07 +0900

    試したり

--- tutorials/tour_of_rust/ch05/53.rs
+++ tutorials/tour_of_rust/ch05/53.rs
@@ -2,6 +2,10 @@ struct Foo {
     x: i32,
 }
 
+fn do_something_0(foo: &Foo) -> i32 {
+    return foo.x;
+}
+
 // 引数 foo と戻り値はライフタイムを共有
 fn do_something<'a>(foo: &'a Foo) -> &'a i32 {
     return &foo.x;
@@ -9,12 +13,17 @@ fn do_something<'a>(foo: &'a Foo) -> &'a i32 {
 
 fn main() {
     let mut foo = Foo { x: 42 };
+    println!("foo.x address: {:p}", &foo.x);
     let x = &mut foo.x;
-
     *x = 13;
-    // x はここでドロップされるため不変な参照が作成可能
-    let y = do_something(&foo);
-    println!("{}", y);
-    // y はここでドロップ
-    // foo はここでドロップ
+    // x はここでドロップされるため、不変な参照が作成可能
+
+    let y = do_something_0(&foo);
+    println!("y address: {:p}, y: {}", &y, y);
+
+    let z = do_something(&foo);
+    println!("z address: {:p}, z: {}", z, z);
+
+    // z はここでドロップされるため
+    // foo はここでドロップされる
 }
