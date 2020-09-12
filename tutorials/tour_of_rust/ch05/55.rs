214eef95b0d5abf08b3d0788bea2c6c28af2d546
makutak <takkouno@gmail.com>
2020-09-13 04:12:58 +0900

    書き直した

--- tutorials/tour_of_rust/ch05/55.rs
+++ tutorials/tour_of_rust/ch05/55.rs
@@ -6,6 +6,10 @@
 この定義では、スタティックライフタイムを持つリソースは実行時にも作成できることに注意。
 
 スタティックライフタイムを持つリソースには、特別なライフタイム指定子 'static がある。
+'static リソースは決してドロップすることはない。
+
+スタティックライフタイムを持つリソースが参照を含む場合、それらはすべて'staticでないといけない。
+(そうでなければ、参照はプログラムの終了前にドロップする可能性がある)
  */
 
 static PI: f64 = 3.1415;
@@ -16,13 +20,16 @@ fn main() {
 
     // 文字列リテラルは 'static ライフタイム
     let msg: &'static str = "Hello World!";
+
     let p: &'static f64 = &PI;
     println!("{} {}", msg, p);
 
-    // ルールを破ることはできますが、それを明示する必要があります。
+    // ルールを破ることはできるが、それを明示する必要がある
     unsafe {
+        println!("SECRET: {}", SECRET);
+
         // 文字列リテラルは 'static なので SECRET に代入可能
         SECRET = "abracadabra";
-        println!("{}", SECRET);
+        println!("SECRET: {}", SECRET);
     }
 }
