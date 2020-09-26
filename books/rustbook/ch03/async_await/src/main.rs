08343228946c05a6c16a53f14c62d7d38fca29c6
makutak <takkouno@gmail.com>
2020-09-26 19:26:02 +0900

    asyncブロックにmoveを使用して所有権を移すことができる

--- books/rustbook/ch03/async_await/src/main.rs
+++ books/rustbook/ch03/async_await/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     executor::block_on(something_great_async_function_2());
     executor::block_on(something_great_async_function_3());
     executor::block_on(caluculate());
+    executor::block_on(move_to_async_block());
 }
 
 async fn async_add(left: i32, right: i32) -> i32 {
@@ -54,6 +55,15 @@ async fn caluculate() -> i32 {
     // これは評価される
     print_result(add2).await;
     async_add(add1, add2).await
+}
+
+fn move_to_async_block() -> impl Future<Output = ()> {
+    let outside_valiable = "this is outside".to_string();
+    // 通常ならここで、outside_variable の所有権がなくなるのでコンパイルは通らないが、
+    async move {
+        // move を用いると、変数の所有権を async ブロックの中に移し、ブロック内でも使用できる。
+        println!("{}", outside_valiable);
+    }
 }
 
 // async ではないので下記はコンパイルエラーとなる。
