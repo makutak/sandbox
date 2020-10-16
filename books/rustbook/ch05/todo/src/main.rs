18d2dba4fe7dec678db80ac6ce820129a5021f5c
makutak <takkouno@gmail.com>
2020-10-17 01:26:25 +0900

    ひとまずテンプレートを表示する

--- books/rustbook/ch05/todo/src/main.rs
+++ books/rustbook/ch05/todo/src/main.rs
@@ -1,15 +1,26 @@
 use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
+use askama::Template;
 use thiserror::Error;
 
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {}
+
 #[derive(Error, Debug)]
-enum MyError {}
+enum MyError {
+    #[error("Failed to render HTML")]
+    AsakamaError(#[from] askama::Error),
+}
 
 impl ResponseError for MyError {}
 
 #[get("/")]
 async fn index() -> Result<HttpResponse, MyError> {
-    let response_body = "Hello world!";
-    Ok(HttpResponse::Ok().body(response_body))
+    let html = IndexTemplate {};
+    let response_body = html.render()?;
+    Ok(HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response_body))
 }
 
 #[actix_rt::main]
