379c53e21d607142fe3f6f02d99aa56e2f91f98a
makutak <takkouno@gmail.com>
2020-11-01 01:26:25 +0900

    textを追加できるように

--- books/rustbook/ch05/todo/src/main.rs
+++ books/rustbook/ch05/todo/src/main.rs
@@ -1,12 +1,17 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, ResponseError};
+use actix_web::{get, http::header, post, web, App, HttpResponse, HttpServer, ResponseError};
 use askama::Template;
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
 use rusqlite::params;
+use serde::Deserialize;
 use thiserror::Error;
 
 struct TodoEntry {
     id: u32,
+    text: String,
+}
+#[derive(Deserialize)]
+struct AddParams {
     text: String,
 }
 
@@ -53,6 +58,18 @@ async fn index(db: web::Data<Pool<SqliteConnectionManager>>) -> Result<HttpRespo
         .body(response_body))
 }
 
+#[post("/add")]
+async fn add_todo(
+    params: web::Form<AddParams>,
+    db: web::Data<Pool<SqliteConnectionManager>>,
+) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    conn.execute("INSERT INTO todo (text) values (?)", &[&params.text])?;
+    Ok(HttpResponse::SeeOther()
+        .header(header::LOCATION, "/")
+        .finish())
+}
+
 #[actix_rt::main]
 async fn main() -> Result<(), actix_web::Error> {
     let manager = SqliteConnectionManager::file("todo.db");
@@ -69,9 +86,14 @@ CREATE TABLE IF NOT EXISTS todo (
     )
     .expect("Failed to create a table `todo`.");
 
-    HttpServer::new(move || App::new().service(index).data(pool.clone()))
-        .bind("0.0.0.0:8080")?
-        .run()
-        .await?;
+    HttpServer::new(move || {
+        App::new()
+            .service(index)
+            .service(add_todo)
+            .data(pool.clone())
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await?;
     Ok(())
 }
