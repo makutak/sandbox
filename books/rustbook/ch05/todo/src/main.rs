7eda497ee2dda581e9a14e88a9b7952e7189a7d7
makutak <takkouno@gmail.com>
2020-11-11 03:59:23 +0900

    エラーを定義

--- books/rustbook/ch05/todo/src/main.rs
+++ books/rustbook/ch05/todo/src/main.rs
@@ -1,4 +1,10 @@
 use actix_web::{get, App, HttpResponse, HttpServer};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum MyError {}
+
+impl ResponseError for MyError {}
 
 #[get("/")]
 async fn index() -> Result<HttpResponse, actix_web::Error> {
