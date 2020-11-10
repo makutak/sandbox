d50a996e17ef0fdf724e06d732d8c2c06127a1e4
makutak <takkouno@gmail.com>
2020-11-11 01:26:25 +0900

    各セルについての色情報を格納する関数

--- books/rustbook/ch06/mandelbrot/src/logic.rs
+++ books/rustbook/ch06/mandelbrot/src/logic.rs
@@ -14,3 +14,32 @@ fn get_n_diverged(x0: f64, y0: f64, max_iter: usize) -> u8 {
     }
     max_iter as u8
 }
+
+pub fn generate_mandelbrot_set(
+    canvas_w: usize,
+    canvas_h: usize,
+    x_min: f64,
+    x_max: f64,
+    y_min: f64,
+    y_max: f64,
+    max_iter: usize,
+) -> Vec<u8> {
+    let canvas_w_f64 = canvas_w as f64;
+    let canvas_h_f64 = canvas_h as f64;
+    // javascriptの8ビット符号なし整数の配列であるUint8ClampedArray型をつくりたいため Vec<u8> で色情報を作る
+    let mut data = vec![];
+    for i in 0..canvas_h {
+        let i_f64 = i as f64;
+        let y = y_min + (y_max - y_min) * i_f64 / canvas_h_f64;
+        for j in 0..canvas_w {
+            let x = x_min + (x_max - x_min) * j as f64 / canvas_w_f64;
+            let iter_index = get_n_diverged(x, y, max_iter);
+            let v = iter_index % 8 * 32; //8色に塗り分ける
+            data.push(v); // R
+            data.push(v); // G
+            data.push(v); // B
+            data.push(255); // A
+        }
+    }
+    data
+}
