fe7551551cf5a92312e782a710cb184588719d1e
makutak <takkouno@gmail.com>
2020-12-03 04:01:18 +0900

    ひとまず初期値を表示する

--- books/rustbook/ch07/ui_demo/src/main.rs
+++ books/rustbook/ch07/ui_demo/src/main.rs
@@ -3,6 +3,11 @@ use iced::{
     HorizontalAlignment, Length, Row, Settings, Subscription, Text,
 };
 
+const FONT: Font = Font::External {
+    name: "PixelMplus12-Regular",
+    bytes: include_bytes!("../rsc/PixelMplus12-Regular.ttf"),
+};
+
 struct GUI {
     start_stop_button_state: button::State,
     reset_button_state: button::State,
@@ -32,7 +37,38 @@ impl Application for GUI {
     }
 
     fn view(&mut self) -> Element<Self::Message> {
-        Text::new("Hello, World!").into()
+        // init widgets
+        let tick_text = Text::new("00:00:00.00").font(FONT).size(60);
+        let start_stop_button = Button::new(
+            &mut self.start_stop_button_state,
+            Text::new("Start")
+                .horizontal_alignment(HorizontalAlignment::Center)
+                .font(FONT),
+        )
+        .min_width(80);
+        let reset_button = Button::new(
+            &mut self.reset_button_state,
+            Text::new("Reset")
+                .horizontal_alignment(HorizontalAlignment::Center)
+                .font(FONT),
+        )
+        .min_width(80);
+
+        // prepare column
+        Column::new()
+            .push(tick_text)
+            .push(
+                Row::new()
+                    .push(start_stop_button)
+                    .push(reset_button)
+                    .spacing(10),
+            )
+            .spacing(10)
+            .padding(10)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .align_items(Align::Center)
+            .into()
     }
 }
 
