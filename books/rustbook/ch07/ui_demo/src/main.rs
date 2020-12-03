6da6bb5876eee06a7ac5ade7513040fd1768e1ee
makutak <takkouno@gmail.com>
2020-12-04 04:45:21 +0900

    ボタンを押したら表示が変わるように

--- books/rustbook/ch07/ui_demo/src/main.rs
+++ books/rustbook/ch07/ui_demo/src/main.rs
@@ -46,27 +46,53 @@ impl Application for GUI {
         String::from("DEMO")
     }
 
-    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
+    fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
+        match message {
+            Message::Start => {
+                self.tick_state = TickState::Ticking;
+            }
+            Message::Stop => {
+                self.tick_state = TickState::Stopped;
+            }
+            Message::Reset => {}
+        }
         Command::none()
     }
 
     fn view(&mut self) -> Element<Self::Message> {
-        // init widgets
-        let tick_text = Text::new("00:00:00.00").font(FONT).size(60);
-        let start_stop_button = Button::new(
-            &mut self.start_stop_button_state,
-            Text::new("Start")
+        // prepare duration text
+        let duration_text = "00:00:00.00";
+
+        // prepare start/stop text
+        let start_stop_text = match self.tick_state {
+            TickState::Stopped => Text::new("Start")
                 .horizontal_alignment(HorizontalAlignment::Center)
                 .font(FONT),
-        )
-        .min_width(80);
+            TickState::Ticking => Text::new("Stop")
+                .horizontal_alignment(HorizontalAlignment::Center)
+                .font(FONT),
+        };
+
+        // prepare start/stop message on button press
+        let start_stop_message = match self.tick_state {
+            TickState::Stopped => Message::Start,
+            TickState::Ticking => Message::Stop,
+        };
+
+        // init widgets
+        let tick_text = Text::new(duration_text).font(FONT).size(60);
+        let start_stop_button = Button::new(&mut self.start_stop_button_state, start_stop_text)
+            .min_width(80)
+            .on_press(start_stop_message);
+
         let reset_button = Button::new(
             &mut self.reset_button_state,
             Text::new("Reset")
                 .horizontal_alignment(HorizontalAlignment::Center)
                 .font(FONT),
         )
-        .min_width(80);
+        .min_width(80)
+        .on_press(Message::Reset);
 
         // prepare column
         Column::new()
