32268059f8f404fe03f739d7f0826827d2ac3dd1
makutak <takkouno@gmail.com>
2020-12-08 05:03:24 +0900

    コントロールグループの作成

--- labo/container/container.sh
+++ labo/container/container.sh
@@ -7,3 +7,11 @@ echo CID: $CID
 sudo docker container export $CID | tar -x -C $ROOTFS
 ln -s /usr/local/bin/bash $ROOTFS/bin/bash
 sudo docker container rm $CID
+
+UUID=$(uuidgen)
+sudo cgcreate -t $(id -un):$(id -gn) -a $(id -un):$(id -gn) -g cpu,memory:$UUID
+cgset -r memory.limit_in_bytes=10000000 $UUID
+cgset -r cpu.cfm_preriod_us=10000000 $UUID
+cgset -r cpu.cfg_quota_us=300000 $UUID
+#https://access.redhat.com/documentation/ja-jp/red_hat_enterprise_linux/6/html/resource_management_guide/sec-creating_cgroups
+
