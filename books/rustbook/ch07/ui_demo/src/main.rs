8b772441b38efde2262e171509a37f5b65c00965
makutak <takkouno@gmail.com>
2020-12-09 04:36:35 +0900

    時間を表示させる

--- books/rustbook/ch07/ui_demo/src/main.rs
+++ books/rustbook/ch07/ui_demo/src/main.rs
@@ -30,6 +30,8 @@ pub enum TickState {
 }
 
 struct GUI {
+    last_update: Instant,
+    total_duration: Duration,
     tick_state: TickState,
     start_stop_button_state: button::State,
     reset_button_state: button::State,
@@ -43,6 +45,8 @@ impl Application for GUI {
     fn new(_flags: ()) -> (GUI, Command<Self::Message>) {
         (
             GUI {
+                last_update: Instant::now(),
+                total_duration: Duration::default(),
                 tick_state: TickState::Stopped,
                 start_stop_button_state: button::State::new(),
                 reset_button_state: button::State::new(),
@@ -59,18 +63,38 @@ impl Application for GUI {
         match message {
             Message::Start => {
                 self.tick_state = TickState::Ticking;
+                self.last_update = Instant::now();
             }
             Message::Stop => {
                 self.tick_state = TickState::Stopped;
+                self.total_duration += Instant::now() - self.last_update;
             }
-            Message::Reset => {}
+            Message::Reset => {
+                self.last_update = Instant::now();
+                self.total_duration = Duration::default();
+            }
+            Message::Update => match self.tick_state {
+                TickState::Ticking => {
+                    let now_update = Instant::now();
+                    self.total_duration += now_update - self.last_update;
+                    self.last_update = now_update;
+                }
+                _ => {}
+            },
         }
         Command::none()
     }
 
     fn view(&mut self) -> Element<Self::Message> {
         // prepare duration text
-        let duration_text = "00:00:00.00";
+        let seconds = self.total_duration.as_secs();
+        let duration_text = format!(
+            "{:0>2}:{:0>2}:{:0>2}.{:0>2}",
+            seconds / HOUR,
+            (seconds & HOUR) / MINUTE,
+            seconds % MINUTE,
+            self.total_duration.subsec_millis() / 10
+        );
 
         // prepare start/stop text
         let start_stop_text = match self.tick_state {
