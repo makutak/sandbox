82a7815c8c7ac7abf652b634b8a2514356633181
makutak <takkouno@gmail.com>
2020-12-30 03:27:17 +0900

    とりあえず引数をパースする

--- books/network-rust/port-scanner/src/main.rs
+++ books/network-rust/port-scanner/src/main.rs
@@ -1,3 +1,39 @@
+use clap::{App, Arg};
+
+use std::env;
+
+#[macro_use]
+extern crate log;
+
+#[macro_use]
+extern crate clap;
+
 fn main() {
-    println!("Hello, world!");
+    env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(
+            Arg::new("target_ipaddr")
+                .about("[ipaddr]")
+                .value_name("IP_ADDR")
+                .index(1)
+                .required(true),
+        )
+        .arg(
+            Arg::new("scantype")
+                .about("[scantype]")
+                .value_name("SCAN TYPE")
+                .index(2)
+                .required(true),
+        )
+        .get_matches();
+
+    let target_ipaddr = matches.value_of("target_ipaddr").unwrap();
+    let scan_type = matches.value_of("scantype").unwrap();
+
+    println!("target_iparrd: {}, scan_type: {}", target_ipaddr, scan_type);
 }
