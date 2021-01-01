2c192ea65467ff5e249cab8c6f1432887c255b35
makutak <takkouno@gmail.com>
2021-01-02 03:33:43 +0900

    とりあえずUDPの67番ポートで接続を待ち受ける

--- books/network-rust/dhcp_server/src/main.rs
+++ books/network-rust/dhcp_server/src/main.rs
@@ -1,3 +1,24 @@
+use std::{env, net::UdpSocket};
+
+#[macro_use]
+extern crate log;
+
 fn main() {
-    println!("Hello, world!");
+    env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+
+    let server_socket = UdpSocket::bind("0.0.0.0:67").expect("Failed to bind socket");
+    server_socket.set_broadcast(true).unwrap();
+
+    loop {
+        let mut recv_buf = [0u8; 1024];
+        match server_socket.recv_from(&mut recv_buf) {
+            Ok((size, src)) => {
+                debug!("received data from {}, size: {}", src, size);
+            }
+            Err(e) => {
+                error!("Could not receive a datagram: {}", e);
+            }
+        }
+    }
 }
