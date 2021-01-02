ffbac5dd542837e8c0b4087b9e2e235ecc98e009
makutak <takkouno@gmail.com>
2021-01-03 06:38:18 +0900

    add getter

--- books/network-rust/dhcp_server/src/dhcp.rs
+++ books/network-rust/dhcp_server/src/dhcp.rs
@@ -1,3 +1,7 @@
+use std::net::Ipv4Addr;
+
+use pnet::util::MacAddr;
+
 const OP: usize = 0;
 const HTYPE: usize = 1;
 const HLEN: usize = 2;
@@ -19,4 +23,41 @@ const OPTIONS: usize = 236;
  */
 pub struct DhcpPacket {
     buffer: Vec<u8>,
+}
+
+impl DhcpPacket {
+    pub fn get_buffer(&self) -> &[u8] {
+        self.buffer().as_ref()
+    }
+
+    pub fn get_op(&self) -> u8 {
+        self.buffer[OP]
+    }
+
+    pub fn get_options(&self) -> &[u8] {
+        self.buffer[OPTIONS..]
+    }
+
+    pub fn get_xid(&self) -> &[u8] {
+        self.buffer[XID..SECS]
+    }
+
+    pub fn get_flags(&self) -> &[u8] {
+        self.buffer[FLAGS..CIADDR]
+    }
+
+    pub fn get_giaddr(&self) -> Ipv4Addr {
+        let b = &self.buffer[GIADDR..CHADDR];
+        Ipv4Addr::new(b[0], b[1], b[2], b[3])
+    }
+
+    pub fn get_chaddr(&self) -> MacAddr {
+        let b = &self.buffer[CHADDR..SNAME];
+        MacAddr::new(b[0], b[1], b[2], b[3], b[4], b[5])
+    }
+
+    pub fn get_ciaddr(&self) -> Ipv4Addr {
+        let b = &self.buffer[CIADDR..YIADDR];
+        Ipv4Addr::new(b[0], b[1], b[2], b[3])
+    }
 }
