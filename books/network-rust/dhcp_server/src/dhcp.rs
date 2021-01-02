95e208906b2c0da4503476337b4e6f23bbede745
makutak <takkouno@gmail.com>
2021-01-03 06:23:58 +0900

    パケットフォーマットのフィールド定数を定義

--- books/network-rust/dhcp_server/src/dhcp.rs
+++ books/network-rust/dhcp_server/src/dhcp.rs
@@ -1,3 +1,19 @@
+const OP: usize = 0;
+const HTYPE: usize = 1;
+const HLEN: usize = 2;
+const HOPS: usize = 3;
+const XID: usize = 4;
+const SECS: usize = 8;
+const FAGS: usize = 10;
+const CIADDR: usize = 12;
+const YIADDR: usize = 16;
+const SIADDR: usize = 20;
+const GIADDR: usize = 24;
+const CHADDR: usize = 28;
+const SNAME: usize = 44;
+const FILE: usize = 108;
+const OPTIONS: usize = 236;
+
 /**
  * DHCPのパケットを表現する
  */
