ccf13190e576684ea8e0264e62daa3b3cf0b595e
makutak <takkouno@gmail.com>
2021-01-15 02:38:58 +0900

    ひとまず実装

--- books/rust-tcp/echoserver/src/main.rs
+++ books/rust-tcp/echoserver/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    env,
+    error::Error,
+    io::{Read, Write},
+    net::TcpListener,
+    str, thread,
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    let addr = &args[1];
+    echo_server(addr)?;
+    Ok(())
+}
+
+fn echo_server(address: &str) -> Result<(), Box<dyn Error>> {
+    let listnner = TcpListener::bind(address)?;
+    loop {
+        let (mut stream, _) = listnner.accept()?;
+        thread::spawn(move || {
+            let mut buffer = [0u8; 1024];
+            loop {
+                let nbytes = stream.read(&mut buffer).unwrap();
+                if nbytes == 0 {
+                    return;
+                }
+                print!("{}", str::from_utf8(&buffer[..nbytes]).unwrap());
+                stream.write_all(&buffer[..nbytes]).unwrap();
+            }
+        });
+    }
 }
