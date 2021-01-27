12851bd02c73fb036983ba578d77a6523b5c85eb
makutak <takkouno@gmail.com>
2021-01-28 04:22:14 +0900

    引数の数チェック

--- labo/git_cat_file_p/src/main.rs
+++ labo/git_cat_file_p/src/main.rs
@@ -7,6 +7,13 @@ use std::{
 };
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() == 1 {
+        eprintln!("Usega: ");
+        eprintln!(" git_cat_file-p blob_file");
+        std::process::exit(1)
+    }
+
     let mut file =
         File::open("/home/tk/work/tmp/repo/.git/objects/3b/18e512dba79e4c8300dd08aeb37f8e728b8dad")
             .unwrap();
