356c1ae33143726082e1a541bf3aad0fa8ce9fa7
makutak <takkouno@gmail.com>
2021-01-29 04:22:14 +0900

    とりあえずzlib解凍して文字列を表示できるが長いとエラーになる

--- labo/git_cat_file_p/src/main.rs
+++ labo/git_cat_file_p/src/main.rs
@@ -7,12 +7,14 @@ use std::{
 };
 
 fn main() -> Result<(), Box<dyn Error>> {
+    /*
     let args: Vec<String> = std::env::args().collect();
     if args.len() == 1 {
         eprintln!("Usega: ");
         eprintln!(" git_cat_file-p blob_file");
         std::process::exit(1)
     }
+    */
 
     let mut file =
         File::open("/home/tk/work/tmp/repo/.git/objects/3b/18e512dba79e4c8300dd08aeb37f8e728b8dad")
@@ -21,11 +23,15 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     file.read_to_end(&mut buf).unwrap();
     println!("{:?}", buf);
-
     let mut decoder = Decoder::new(Cursor::new(&buf)).unwrap();
     let mut decoded_data = Vec::new();
     decoder.read_to_end(&mut decoded_data).unwrap();
-    println!("{:?}", str::from_utf8(&decoded_data).unwrap());
+
+    let str_data = str::from_utf8(&decoded_data).unwrap();
+    println!(
+        "{}",
+        str_data.trim_end().split('\u{0}').collect::<Vec<_>>()[1]
+    );
 
     Ok(())
 }
