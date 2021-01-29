475827bc7480bd98b904e1ca5d30da9069f55a6b
makutak <takkouno@gmail.com>
2021-01-30 06:40:40 +0900

    debugging

--- labo/git_cat_file_p/src/main.rs
+++ labo/git_cat_file_p/src/main.rs
@@ -17,21 +17,25 @@ fn main() -> Result<(), Box<dyn Error>> {
     */
 
     let mut file =
-        File::open("/home/tk/work/tmp/repo/.git/objects/3b/18e512dba79e4c8300dd08aeb37f8e728b8dad")
+        File::open("/home/tk/work/tmp/repo/.git/objects/a4/940ec3db4cd24542203a9447c4259c96294c09")
             .unwrap();
+    // File::open("/home/tk/work/tmp/repo/.git/objects/3b/18e512dba79e4c8300dd08aeb37f8e728b8dad")
+    //     .unwrap();
+
     let mut buf = Vec::new();
 
     file.read_to_end(&mut buf).unwrap();
-    println!("{:?}", buf);
+    //println!("{:?}", buf);
     let mut decoder = Decoder::new(Cursor::new(&buf)).unwrap();
     let mut decoded_data = Vec::new();
     decoder.read_to_end(&mut decoded_data).unwrap();
 
     let str_data = str::from_utf8(&decoded_data).unwrap();
-    println!(
-        "{}",
-        str_data.trim_end().split('\u{0}').collect::<Vec<_>>()[1]
-    );
+    println!("{}", str_data);
+    // println!(
+    //     "{}",
+    //     str_data.trim_end().split('\u{0}').collect::<Vec<_>>()[1]
+    // );
 
     Ok(())
 }
