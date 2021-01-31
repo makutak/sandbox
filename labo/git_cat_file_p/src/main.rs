08e9532ef72af631a566f49b162ad57c5004db96
makutak <takkouno@gmail.com>
2021-02-01 05:07:20 +0900

    shift_jisもzlib解凍できるように

--- labo/git_cat_file_p/src/main.rs
+++ labo/git_cat_file_p/src/main.rs
@@ -1,8 +1,7 @@
 use libflate::zlib::Decoder;
 use std::{
-    error::Error,
     fs::File,
-    io::{Cursor, Read},
+    io::{self, Cursor, Read},
     str,
 };
 
@@ -11,33 +10,42 @@ fn usage() {
     println!("git_cat_file_p blob_file");
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn decompress(mut file: File) -> Result<String, io::Error> {
+    let mut buf = Vec::new();
+    file.read_to_end(&mut buf).unwrap();
+
+    let mut decoder = Decoder::new(Cursor::new(&buf)).unwrap();
+    let mut decoded_data = Vec::new();
+    decoder.read_to_end(&mut decoded_data).unwrap();
+
+    unsafe {
+        let str_data = String::from_utf8_unchecked(decoded_data);
+        Ok(str_data)
+    }
+}
+
+fn format(decoded: &str) -> &str {
+    decoded.trim_end().split('\u{0}').collect::<Vec<_>>()[1]
+}
+
+fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() == 1 {
         usage();
         std::process::exit(1)
     }
 
-    let mut file =
-        File::open("/home/tk/work/tmp/repo/.git/objects/a4/940ec3db4cd24542203a9447c4259c96294c09")
-            .unwrap();
-    // File::open("/home/tk/work/tmp/repo/.git/objects/3b/18e512dba79e4c8300dd08aeb37f8e728b8dad")
-    //     .unwrap();
+    let f =
+        File::open("/home/tk/work/tmp/repo/.git/objects/a4/940ec3db4cd24542203a9447c4259c96294c09");
+    //File::open("/home/tk/work/tmp/repo/.git/objects/3b/18e512dba79e4c8300dd08aeb37f8e728b8dad");
+    //File::open("./tmp.txt");
 
-    let mut buf = Vec::new();
+    let file = match f {
+        Ok(file) => file,
+        Err(error) => panic!("{}: Can't open: {}", error, args[1]),
+    };
 
-    file.read_to_end(&mut buf).unwrap();
-    //println!("{:?}", buf);
-    let mut decoder = Decoder::new(Cursor::new(&buf)).unwrap();
-    let mut decoded_data = Vec::new();
-    decoder.read_to_end(&mut decoded_data).unwrap();
-
-    let str_data = str::from_utf8(&decoded_data).unwrap();
-    println!("{}", str_data);
-    // println!(
-    //     "{}",
-    //     str_data.trim_end().split('\u{0}').collect::<Vec<_>>()[1]
-    // );
-
-    Ok(())
+    let decoded = decompress(file).unwrap();
+    let formatted = format(&decoded);
+    println!("{}", formatted);
 }
