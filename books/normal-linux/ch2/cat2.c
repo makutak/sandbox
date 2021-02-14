66421cc105def1075b81b7ec94fa2fe9ed941d7d
makutak <takkouno@gmail.com>
2021-02-14 20:59:18 +0900

    コマンドライン引数がファイルでなければ標準入力を受け取るように

--- books/normal-linux/ch2/cat2.c
+++ books/normal-linux/ch2/cat2.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static void do_cat(FILE *fd);
+static void die(const char *s);
+
 int main (int argc, char **argv) {
     int i;
 
+    if (argc == 1) do_cat(stdin);
+
     for (i = 1; i < argc; i++) {
         FILE *f;
-        int c;
 
         f = fopen(argv[i], "r");
-        if (f < 0) {
-            perror(argv[i]);
-            exit(1);
-        }
+        if (!f) die(argv[i]);
 
-        while((c = fgetc(f)) != EOF) {
-            if (putchar(c) < 0) exit(1);
-        }
+        do_cat(f);
         fclose(f);
     }
 
     exit(0);
+}
+
+static void do_cat(FILE *fd) {
+    int c;
+
+    while((c = fgetc(fd)) != EOF) {
+        if (putchar(c) < 0) exit(1);
+    }
+}
+
+static void die(const char *s) {
+    perror(s);
+    exit(1);
 }
