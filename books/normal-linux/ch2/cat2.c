30831fce671e2ee8354d55329f900b8cb44865dd
makutak <takkouno@gmail.com>
2021-02-17 05:48:09 +0900

    formatted

--- books/normal-linux/ch2/cat2.c
+++ books/normal-linux/ch2/cat2.c
@@ -4,33 +4,33 @@
 static void do_cat(FILE *fd);
 static void die(const char *s);
 
-int main (int argc, char **argv) {
-    int i;
+int main(int argc, char **argv) {
+  int i;
 
-    if (argc == 1) do_cat(stdin);
+  if (argc == 1) do_cat(stdin);
 
-    for (i = 1; i < argc; i++) {
-        FILE *f;
+  for (i = 1; i < argc; i++) {
+    FILE *f;
 
-        f = fopen(argv[i], "r");
-        if (!f) die(argv[i]);
+    f = fopen(argv[i], "r");
+    if (!f) die(argv[i]);
 
-        do_cat(f);
-        fclose(f);
-    }
+    do_cat(f);
+    fclose(f);
+  }
 
-    exit(0);
+  exit(0);
 }
 
 static void do_cat(FILE *fd) {
-    int c;
+  int c;
 
-    while((c = fgetc(fd)) != EOF) {
-        if (putchar(c) < 0) exit(1);
-    }
+  while ((c = fgetc(fd)) != EOF) {
+    if (putchar(c) < 0) exit(1);
+  }
 }
 
 static void die(const char *s) {
-    perror(s);
-    exit(1);
+  perror(s);
+  exit(1);
 }
