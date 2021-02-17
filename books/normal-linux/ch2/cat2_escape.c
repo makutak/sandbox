2b8b3dd89db837984bd7da2958d6afecf4316fec
makutak <takkouno@gmail.com>
2021-02-18 05:48:09 +0900

    formatted

--- books/normal-linux/ch2/cat2_escape.c
+++ books/normal-linux/ch2/cat2_escape.c
@@ -4,43 +4,43 @@
 static void do_cat(FILE *fd);
 static void die(const char *s);
 
-int main (int argc, char **argv) {
-    int i;
+int main(int argc, char **argv) {
+  int i;
 
-    if (argc == 1) do_cat(stdin);
+  if (argc == 1) do_cat(stdin);
 
-    for (i = 1; i < argc; i++) {
-        FILE *f;
+  for (i = 1; i < argc; i++) {
+    FILE *f;
 
-        f = fopen(argv[i], "r");
-        if (!f) die(argv[i]);
+    f = fopen(argv[i], "r");
+    if (!f) die(argv[i]);
 
-        do_cat(f);
-        fclose(f);
-    }
+    do_cat(f);
+    fclose(f);
+  }
 
-    exit(0);
+  exit(0);
 }
 
 static void do_cat(FILE *fd) {
-    int c;
+  int c;
 
-    while((c = fgetc(fd)) != EOF) {
-        switch (c) {
-        case '\t':
-            if (fputs("\\t", stdout) == EOF) exit(1);
-            break;
-        case '\n':
-            if (fputs("$\n", stdout) == EOF) exit(1);
-            break;
-        default:
-            if (putchar(c) < 0) exit(1);
-            break;
-        }
+  while ((c = fgetc(fd)) != EOF) {
+    switch (c) {
+      case '\t':
+        if (fputs("\\t", stdout) == EOF) exit(1);
+        break;
+      case '\n':
+        if (fputs("$\n", stdout) == EOF) exit(1);
+        break;
+      default:
+        if (putchar(c) < 0) exit(1);
+        break;
     }
+  }
 }
 
 static void die(const char *s) {
-    perror(s);
-    exit(1);
+  perror(s);
+  exit(1);
 }
