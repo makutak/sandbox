faa89509fa96159bebb1c9c365d812d9196e5903
makutak <takkouno@gmail.com>
2021-02-28 05:48:09 +0900

    ひとまず外観

--- books/normal-linux/ch2/tails/tail.c
+++ books/normal-linux/ch2/tails/tail.c
@@ -1,3 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv) { return 0; }
+static void do_tail(FILE *fd);
+
+int main(int argc, char **argv) {
+  int i;
+  FILE *f;
+
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s [FILE]\n", argv[0]);
+    exit(1);
+  }
+
+  f = fopen(argv[1], "r");
+  do_tail(f);
+
+  return 0;
+}
+
+void do_tail(FILE *fd) {
+  printf("do_tail\n");
+  exit(0);
+}
