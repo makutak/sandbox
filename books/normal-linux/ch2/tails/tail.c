c098731eb439a7b94c2a32e463243bebbe6a8ae0
makutak <takkouno@gmail.com>
2021-03-01 05:48:09 +0900

    ひとまず

--- books/normal-linux/ch2/tails/tail.c
+++ books/normal-linux/ch2/tails/tail.c
@@ -1,24 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static void do_tail(FILE *fd);
+#define N_LINES 10
+#define MAX_LINE_LENGTH 1024
+
+static void tail(FILE *f);
 
 int main(int argc, char **argv) {
-  int i;
-  FILE *f;
-
-  if (argc != 2) {
-    fprintf(stderr, "Usage: %s [FILE]\n", argv[0]);
+  if (argc != 1) {
+    fprintf(stderr, "Usage: %s infile", argv[0]);
     exit(1);
   }
 
-  f = fopen(argv[1], "r");
-  do_tail(f);
-
-  return 0;
+  tail(stdin);
+  exit(0);
 }
 
-void do_tail(FILE *fd) {
-  printf("do_tail\n");
-  exit(0);
+static char ring_buffer[N_LINES][MAX_LINE_LENGTH];
+
+#define RING_BUFFER_INC(idx)                    \
+  do {                                          \
+    idx++;                                      \
+    if (idx >= N_LINES) {                       \
+      idx = 0;                                  \
+    }                                           \
+  } while (0)
+
+static void tail(FILE *f) {
+  long curr = 0;
+  long n_read_lines = 0;
+
+  while (fgets(ring_buffer[curr], MAX_LINE_LENGTH, f)) {
+    RING_BUFFER_INC(curr);
+    n_read_lines++;
+  }
+
+  if (n_read_lines < N_LINES) {
+    curr = 0;
+  }
+
+  int n;
+
+  for (n = N_LINES; n > 0 && n_read_lines; n--, n_read_lines--) {
+    printf("%s", ring_buffer[curr]);
+    RING_BUFFER_INC(curr);
+  }
 }
