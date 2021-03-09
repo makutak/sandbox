8c1d41451c2bc930e138d82bceb58783497d470c
makutak <takkouno@gmail.com>
2021-03-10 05:48:09 +0900

    ひとまずエラーハンドリングとか関数定義とか

--- books/stdlinux/ch2/ls/ls.c
+++ books/stdlinux/ch2/ls/ls.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-static void do_ls();
+static void do_ls(char *path);
 
 int main(int argc, char **argv) {
   int i;
 
   if (argc < 2) {
     fprintf(stderr, "%s: no arguments.\n", argv[0]);
+    exit(1);
   }
+
   for (i = 1; i < argc; i++) {
-    printf("%s\n", argv[i]);
+    do_ls(argv[i]);
   }
+
   return 0;
+}
+
+static void do_ls(char *path) {
+  printf("path: %s\n", path);
 }
