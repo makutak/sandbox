3229b0a25c35dac117a6e38b3d8f78b0d6d40f73
makutak <takkouno@gmail.com>
2021-03-19 10:25:05 +0900

    statコマンド途中

---
+++ books/stdlinux/ch2/stat/stat.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+  struct stat st;
+
+  if (argc != 2) {
+    fprintf(stderr, "%s: wrong arguments.\n", argv[0]);
+    exit(1);
+  }
+
+  if (lstat(argv[1], &st) < 0) {
+    perror(argv[1]);
+    exit(1);
+  }
+
+  printf("type: \t%o\n", (st.st_mode & S_IFMT));
+  exit(0);
+}
