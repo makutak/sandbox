c0e4012748bc3acd666c5df249941c79838f97f2
makutak <takkouno@gmail.com>
2021-03-31 01:34:54 +0900

    41まで

--- kata/bowling_game/2021_03_30/src/game.py
+++ kata/bowling_game/2021_03_30/src/game.py
@@ -1,2 +1,18 @@
 class Game:
-    pass
+    def __init__(self):
+        self.__rolls = [0] * 21
+        self.__current_roll = 0
+
+    def roll(self, pins):
+        self.__rolls[self.__current_roll] = pins
+        self.__current_roll += 1
+
+    def score(self):
+        score = 0
+        i = 0
+        for frame in range(10):
+            score += self.__rolls[i] + self.__rolls[i + 1]
+            i += 2
+
+        print(score)
+        return score
--- kata/bowling_game/2021_03_30/tests/test_bowling_game.py
+++ kata/bowling_game/2021_03_30/tests/test_bowling_game.py
@@ -1,6 +1,29 @@
 from src.game import Game
+import pytest
 
 
-class BowlingGameTest:
-    def test_gutter_game():
-        g = Game()
+class TestBowlingGame:
+    def setup_method(self, method):
+        self.g = Game()
+
+    def roll_many(self, n, pins):
+        for i in range(n):
+            self.g.roll(pins)
+
+    def test_gutter_game(self):
+        self.roll_many(20, 0)
+        assert 0 == self.g.score()
+
+    def test_all_one(self):
+        self.roll_many(20, 1)
+        assert 20 == self.g.score()
+
+    def test_one_spare(self):
+        self.g.roll(5)
+        self.g.roll(5)
+        self.g.roll(3)
+        self.roll_many(17, 0)
+        assert 16 == self.g.score()
+
+
+# 41まで
