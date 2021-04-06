180f45471c16a66b9912ca649a3c33bc72401950
makutak <takkouno@gmail.com>
2021-04-06 23:59:52 +0900

    入力部分

--- books/stdlinux/ch2/spawn/sh2.c
+++ books/stdlinux/ch2/spawn/sh2.c
@@ -1,4 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv) { exit(0); }
+
+static void prompt(void);
+
+static char *program_name;
+
+int main(int argc, char **argv) {
+  program_name = argv[0];
+
+  for (;;) {
+    prompt();
+  }
+
+  exit(0);
+}
+
+#define LINEBUF_MAX 2048
+
+static void prompt(void) {
+  static char buf[LINEBUF_MAX];
+
+  fprintf(stdout, "$ ") ;
+  fflush(stdout);
+
+  if (fgets(buf, LINEBUF_MAX, stdin) == NULL)
+    exit(0);
+}
