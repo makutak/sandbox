b27cc65968638f5538aea574855217950f8e991c
makutak <takkouno@gmail.com>
2021-04-25 23:58:36 +0900

    メモリ管理

--- books/stdlinux/ch3/http_server/server.c
+++ books/stdlinux/ch3/http_server/server.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 static void log_exit(char *fmt, ...);
+static void* xmalloc(size_t sz);
 
 int main(int argc, char **argv) { return 0; }
 
@@ -15,4 +16,13 @@ static void log_exit(char *fmt, ...) {
   fputc('\n', stderr);
   va_end(ap);
   exit(1);
+}
+
+static void* xmalloc(size_t sz) {
+  void *p;
+
+  p = malloc(sz);
+
+  if (!p) log_exit("failed to allocate memory");
+  return p;
 }
