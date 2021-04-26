4ff812523a5d96f371cdbf4e674de6754eab1d32
makutak <takkouno@gmail.com>
2021-04-26 23:58:36 +0900

    シグナル処理

--- books/stdlinux/ch3/http_server/server.c
+++ books/stdlinux/ch3/http_server/server.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <errno.h>
 
 static void log_exit(char *fmt, ...);
 static void* xmalloc(size_t sz);
+static void install_signal_handlers(void);
+static void trap_signal(int sig, __sighandler_t handler);
+static void signal_exit(int sig);
 
 int main(int argc, char **argv) { return 0; }
 
@@ -26,3 +32,20 @@ static void* xmalloc(size_t sz) {
   if (!p) log_exit("failed to allocate memory");
   return p;
 }
+
+static void install_signal_handlers(void) {
+  trap_signal(SIGPIPE);
+}
+
+
+static void trap_signal(int sig, __sighandler_t handler) {
+  struct sigaction act;
+
+  act.sa_handler = handler;
+  sigemptyset(&act.sa_mask);
+  act.sa_flags = SA_RESTART;
+  if (sigaction(sig, &act, NULL) < 0)
+    log_exit("sigaction() failed: %s", strerror(errno));
+}
+
+static void signal_exit(int sig) { log_exit("exit by signal %d", sig); }
