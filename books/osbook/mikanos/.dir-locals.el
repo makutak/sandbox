fb3e86a71a4be05cdbd2664276839fe890d382a4
makutak <takkouno@gmail.com>
2021-05-16 23:58:36 +0900

    ローカルプロジェクトのinclude pathを設定

---
+++ books/osbook/mikanos/.dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((eval . (setq flycheck-clang-include-path
+                       (list (expand-file-name "~/edk2/MdePkg/Include")
+                             (expand-file-name "~/edk2/MdePkg/Include/X64")))))))
