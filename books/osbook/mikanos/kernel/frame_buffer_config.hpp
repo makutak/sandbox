38218d62473f7adcd7b9393e9a4f3ad77f145d8e
makutak <takkouno@gmail.com>
2021-06-17 23:58:36 +0900

    fix typo

--- books/osbook/mikanos/kernel/frame_buffer_config.hpp
+++ books/osbook/mikanos/kernel/frame_buffer_config.hpp
@@ -10,7 +10,7 @@ enum PixelFormat {
 struct FrameBufferConfig {
   uint8_t* frame_buffer;
   uint32_t pixels_per_scan_line;
-  uint32_t horizonal_resolution;
+  uint32_t horizontal_resolution;
   uint32_t vertical_resolution;
   enum PixelFormat pixel_format;
 };
