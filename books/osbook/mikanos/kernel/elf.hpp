ace9a7679bd9d22d0bd1545b03584542548e0661
makutak <takkouno@gmail.com>
2021-06-22 03:50:39 +0900

    add comment

--- books/osbook/mikanos/kernel/elf.hpp
+++ books/osbook/mikanos/kernel/elf.hpp
@@ -18,12 +18,12 @@ typedef struct {
   Elf64_Half    e_machine;
   Elf64_Word    e_version;
   Elf64_Addr    e_entry;
-  Elf64_Off     e_phoff;
+  Elf64_Off     e_phoff;     // プログラムヘッダのファイルオフセット
   Elf64_Off     e_shoff;
   Elf64_Sword   e_flags;
   Elf64_Half    e_ephsize;
-  Elf64_Half    e_phentsize;
-  Elf64_Half    e_phnum;
+  Elf64_Half    e_phentsize; // 要素一つの大きさ
+  Elf64_Half    e_phnum;     // 要素数
   Elf64_Half    e_shentsize;
   Elf64_Half    e_shnum;
   Elf64_Half    e_shstrndx;
