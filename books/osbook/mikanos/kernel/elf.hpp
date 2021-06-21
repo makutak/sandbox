8d6d8f82989e3160bdc796776848e9b87c15b874
makutak <takkouno@gmail.com>
2021-06-22 03:42:42 +0900

    format

--- books/osbook/mikanos/kernel/elf.hpp
+++ books/osbook/mikanos/kernel/elf.hpp
@@ -3,12 +3,12 @@
 #include <stdint.h>
 
 typedef uintptr_t Elf64_Addr;
-typedef uint64_t Elf64_Off;
-typedef uint16_t Elf64_Half;
-typedef uint32_t Elf64_Word;
-typedef int32_t Elf64_Sword;
-typedef uint64_t Elf64_Xword;
-typedef int64_t Elf64_Sxword;
+typedef uint64_t  Elf64_Off;
+typedef uint16_t  Elf64_Half;
+typedef uint32_t  Elf64_Word;
+typedef int32_t   Elf64_Sword;
+typedef uint64_t  Elf64_Xword;
+typedef int64_t   Elf64_Sxword;
 
 #define EI_NIDNET 16
 
