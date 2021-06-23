413d77eeb784a244a5d55d1e2a98b1c6d97155cd
makutak <takkouno@gmail.com>
2021-06-23 23:58:36 +0900

    定義追加

--- books/osbook/mikanos/kernel/elf.hpp
+++ books/osbook/mikanos/kernel/elf.hpp
@@ -39,3 +39,37 @@ typedef struct {
   Elf64_Xword p_memsz;  // メモリサイズ
   Elf64_Xword p_align;
 } Elf64_Phdr;
+
+#define PT_NULL    0
+#define PT_LOAD    1
+#define PT_DYNAMIC 2
+#define PT_INTERP  3
+#define PT_NOTE    4
+#define PT_SHLIB   5
+#define PT_PHDR    6
+#define PT_TLS     7
+
+typedef struct {
+  Elf64_Sword d_tag;
+  union {
+    Elf64_Xword d_val;
+    Elf64_Addr d_ptr;
+  } d_un;
+} Elf64_Dyn;
+
+#define DT_NULL    0
+#define DT_RELA    7
+#define DT_RELASZ  8
+#define DT_RELAENT 9
+
+typedef struct {
+  Elf64_Addr r_offset;
+  Elf64_Xword r_info;
+  Elf64_Sxword r_addend;
+} Elf64_Rela;
+
+#define ELF64_R_SYM(i) ((i)>>32)
+#define ELF64_R_TYPE(i) ((i)&0xffffffffL)
+#define ELF64_R_INFO(s, t) (((s)<<32)+((t)&0xffffffffL))
+
+#define R_X86_64_RELATIVE 8
