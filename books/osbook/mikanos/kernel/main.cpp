f63484712488b928e6f5ddf32172445cd81a6b05
makutak <takkouno@gmail.com>
2021-06-26 13:26:54 +0900

    hankaku.o のデータを扱えるように

--- books/osbook/mikanos/kernel/main.cpp
+++ books/osbook/mikanos/kernel/main.cpp
@@ -29,12 +29,18 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
       pixel_writer->Write(x, y, {255, 255, 255});
     }
   }
+
   for (int x = 0; x < 200; ++x) {
     for (int y = 0; y < 100; ++y) {
       pixel_writer->Write(x, y, {0, 255, 0});
     }
   }
-  WriteAscii(*pixel_writer, 50, 50, 'A', {0, 0, 0});
-  WriteAscii(*pixel_writer, 58, 50, 'A', {0, 0, 0});
+
+  int i = 0;
+  for (char c = '!'; c <= '~'; ++c, ++i) {
+    WriteAscii(*pixel_writer, 8 * i, 50,  c, {0, 0, 0});
+  }
+
+
   while (1) __asm__("hlt");
 }
