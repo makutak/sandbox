29a92b5e0c5613b2bc154d1fcae143f704d07969
makutak <takkouno@gmail.com>
2021-07-01 08:39:34 +0900

    IOポートアドレスの定義

--- books/osbook/mikanos/kernel/pci.hpp
+++ books/osbook/mikanos/kernel/pci.hpp
@@ -1,3 +1,8 @@
 #pragma once
 
 #include <cstdint>
+
+/** CONFIG_ADDRESS レジスタの IO ポートアドレス*/
+const uint16_t kConfigAddress = 0x0cf8;
+/** CONFIG_DATA レジスタの IO ポートアドレス*/
+const uint16_t kConfigData = 0x0cfc;
