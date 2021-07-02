dfc1513cabc64b389463cc2a6c7325fdcd9898dd
makutak <takkouno@gmail.com>
2021-07-03 06:45:33 +0900

    PCIレジスタ操作用の関数

--- books/osbook/mikanos/kernel/pci.cpp
+++ books/osbook/mikanos/kernel/pci.cpp
@@ -6,6 +6,9 @@
 /** @brief CONFIG_ADDRESS 用の 32 ビット整数を生成する */
 #include "pci.hpp"
 
+#include "asmfunc.h"
+
+
 uint32_t MakeAddress(uint8_t bus, uint8_t device, uint8_t function,
                      uint8_t reg_addr) {
   auto shl = [](uint32_t x, unsigned int bits) {
@@ -16,4 +19,21 @@ uint32_t MakeAddress(uint8_t bus, uint8_t device, uint8_t function,
       | shl(device, 11)
       | shl(function, 8)
       | (reg_addr & 0xfcu);
+}
+
+void WriteAddress(uint32_t address) {
+  IoOut32(kConfigAddress, address);
+}
+
+void WriteData(uint32_t value) {
+  IoOut32(kConfigData, value);
+}
+
+uint32_t ReadData() {
+  return IoIn32(kConfigData);
+}
+
+uint16_t ReadVendorId(uint8_t bus, uint8_t device, uint8_t function) {
+  WriteAddress(MakeAddress(bus, device, function, 0x00));
+  return ReadData() & 0xffffu;
 }
