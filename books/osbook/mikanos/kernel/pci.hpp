87584c9bfbd80a394d02704154641f19e4737fa9
makutak <takkouno@gmail.com>
2021-07-04 06:45:33 +0900

    PCIデバイスを登録する配列をグローバル変数として定義

--- books/osbook/mikanos/kernel/pci.hpp
+++ books/osbook/mikanos/kernel/pci.hpp
@@ -1,8 +1,21 @@
 #pragma once
 
 #include <cstdint>
+#include <array>
+
+namespace pci {
 
 /** CONFIG_ADDRESS レジスタの IO ポートアドレス*/
 const uint16_t kConfigAddress = 0x0cf8;
 /** CONFIG_DATA レジスタの IO ポートアドレス*/
 const uint16_t kConfigData = 0x0cfc;
+
+struct Device {
+  uint8_t bus, device, function, header_type;
+};
+
+inline std::array<Device, 32> devices;
+inline int num_device;
+
+//Error ScanAllBus()
+}
