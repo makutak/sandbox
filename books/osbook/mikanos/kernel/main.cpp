af84add9b4e60269db71dc84a5ad93fde98d3414
makutak <takkouno@gmail.com>
2021-07-26 06:45:33 +0900

    xHCの初期化と起動

--- books/osbook/mikanos/kernel/main.cpp
+++ books/osbook/mikanos/kernel/main.cpp
@@ -1,12 +1,18 @@
 #include <cstdint>
 #include <cstddef>
 #include <cstdio>
+
 #include "frame_buffer_config.hpp"
 #include "graphics.hpp"
 #include "font.hpp"
 #include "console.hpp"
 #include "pci.hpp"
 #include "logger.hpp"
+#include "usb/memory.hpp"
+#include "usb/device.hpp"
+#include "usb/classdriver/mouse.hpp"
+#include "usb/xhci/xhci.hpp"
+#include "usb/xhci/trb.hpp"
 
 const PixelColor kDesktopBGColor{45, 118, 237};
 const PixelColor kDesktopFGColor{255, 255, 255};
@@ -63,6 +69,26 @@ int printk(const char *format, ...) {
 
   console->PutString(s);
   return result;
+}
+
+void SwitchEhci2Xhci(const pci::Device &xhc_dev) {
+  bool intel_ehc_exist = false;
+  for (int i = 0; i < pci::num_device; ++i) {
+    if (pci::devices[i].class_code.Match(0x0cu, 0x03u, 0x20u) &&
+        0x8086 == pci::ReadVendorId(pci::devices[i])) {
+      intel_ehc_exist = true;
+      break;
+    }
+  }
+  if (!intel_ehc_exist) {
+    return;
+  }
+
+  uint32_t superspeed_ports = pci::ReadConfReg(xhc_dev, 0xdc);
+  pci::WriteConfReg(xhc_dev, 0xd8, superspeed_ports);
+  uint32_t ehci2xhci_ports = pci::ReadConfReg(xhc_dev,  0xd4);
+  pci::WriteConfReg(xhc_dev, 0xd0, ehci2xhci_ports);
+  Log(kDebug, "SwitchEhci2Xhci: SS = %02, xHCI = %02x\n", superspeed_ports, ehci2xhci_ports);
 }
 
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
@@ -130,10 +156,26 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
   }
 
 
+  // read bar
   const WithError<uint64_t> xhc_bar = pci::ReadBar(*xhc_dev, 0);
   Log(kDebug, "ReadBar: %s\n", xhc_bar.error.Name());
   const uint64_t xhc_mmio_base = xhc_bar.value & ~static_cast<uint64_t>(0xf);
   Log(kDebug, "xHC mmio_base = %08lx\n", xhc_mmio_base);
+
+  // init xhc
+  usb::xhci::Controller xhc {xhc_mmio_base};
+
+  // intel製のxHCだった場合にのみ初期化する前に特別対応をする
+  if (0x8086 == pci::ReadVendorId(*xhc_dev)) {
+    SwitchEhci2Xhci(*xhc_dev);
+  }
+  {
+    auto err = xhc.Initialize();
+    Log(kDebug, "xhx.Initialize: %s\n", err.Name());
+  }
+
+  Log(kInfo, "xHC starting\n");
+  xhc.Run();
 
   while (1) __asm__("hlt");
 }
