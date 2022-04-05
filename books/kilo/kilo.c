d252ed2b9a1f9ead1a90c8a426435528a6c0fea7
makutak <takkouno@gmail.com>
2022-04-06 03:33:51 +0900

    disable raw mode at exit.

--- books/kilo/kilo.c
+++ books/kilo/kilo.c
@@ -1,10 +1,18 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
-void enable_raw_mode() {
-  struct termios raw;
+struct termios orig_termios;
 
-  tcgetattr(STDIN_FILENO, &raw);
+void disable_raw_mode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enable_raw_mode() {
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disable_raw_mode);
+
+  struct termios raw = orig_termios;
 
   raw.c_lflag &= ~(ECHO);
 
