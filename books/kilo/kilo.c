9eaf1a9a10d3bc24fe7d4f5f747fd1bb73bec446
makutak <takkouno@gmail.com>
2022-04-08 08:57:17 +0900

    display keypreses

--- books/kilo/kilo.c
+++ books/kilo/kilo.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -24,7 +26,13 @@ int main() {
   enable_raw_mode();
 
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+      printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n", c, c);
+    }
+  }
 
   return 0;
 }
