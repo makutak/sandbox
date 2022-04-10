efd9612f949435b936f92fa42d019fe9b918e9dd
makutak <takkouno@gmail.com>
2022-04-11 03:32:12 +0900

    refactor keyboard input

--- books/kilo/kilo.c
+++ books/kilo/kilo.c
@@ -39,7 +39,27 @@ void enable_raw_mode() {
   raw.c_cc[VTIME] = 1;
 
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+}
 
+char editor_read_key() {
+  int nread;
+  char c;
+
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) die("read");
+  }
+
+  return c;
+}
+
+void editor_process_keypress() {
+  char c = editor_read_key();
+
+  switch (c) {
+  case CTRL_KEY('q'):
+    exit(0);
+    break;
+  }
 }
 
 /* init */
@@ -48,15 +68,7 @@ int main() {
   enable_raw_mode();
 
   while (1) {
-    char c = '\0';
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-
-    if (iscntrl(c)) {
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
-    }
-    if(c == CTRL_KEY('q')) break;
+    editor_process_keypress();
   }
 
   return 0;
