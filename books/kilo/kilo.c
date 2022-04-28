b8d24806bfb66ad80797b75c356f168c66cf92ab
makutak <takkouno@gmail.com>
2022-04-28 20:12:49 +0900

    enable to read home key and end key

--- books/kilo/kilo.c
+++ books/kilo/kilo.c
@@ -86,8 +86,12 @@ int editor_read_key() {
         if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
         if (seq[2] == '~') {
           switch (seq[1]) {
+            case '1': return HOME_KEY;
+            case '4': return END_KEY;
             case '5': return PAGE_UP;
             case '6': return PAGE_DOWN;
+            case '7': return HOME_KEY;
+            case '8': return END_KEY;
           }
         }
       } else {
@@ -96,9 +100,17 @@ int editor_read_key() {
           case 'B': return ARROW_DOWN;
           case 'C': return ARROW_RIGHT;
           case 'D': return ARROW_LEFT;
+          case 'H': return HOME_KEY;
+          case 'F': return END_KEY;
         }
       }
+    } else if (seq[0] == 'O') {
+      switch (seq[1]) {
+        case 'H': return HOME_KEY;
+        case 'F': return END_KEY;
+      }
     }
+
     return '\x1b';
   } else {
     return c;
