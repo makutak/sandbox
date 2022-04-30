d3b84bad88f00dc85a8df48b7700cf12db566612
makutak <takkouno@gmail.com>
2022-05-01 02:01:22 +0900

    add escape sequence for DEL_KEY

--- books/kilo/kilo.c
+++ books/kilo/kilo.c
@@ -88,6 +88,7 @@ int editor_read_key() {
         if (seq[2] == '~') {
           switch (seq[1]) {
             case '1': return HOME_KEY;
+            case '3': return DEL_KEY;
             case '4': return END_KEY;
             case '5': return PAGE_UP;
             case '6': return PAGE_DOWN;
