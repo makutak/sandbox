a37fa900b7c8cdba92d1785055ccc5a5d66e6715
makutak <takkouno@gmail.com>
2022-05-18 00:52:07 +0900

    move previous line when press left at the beginning of the line.

--- books/kilo/kilo.c
+++ books/kilo/kilo.c
@@ -298,6 +298,9 @@ void editor_move_cursor(int key) {
     case ARROW_LEFT:
       if (E.cx != 0) {
         E.cx--;
+      } else if (E.cy > 0) {
+        E.cy--;
+        E.cx = E.row[E.cy].size;
       }
       break;
     case ARROW_RIGHT:
