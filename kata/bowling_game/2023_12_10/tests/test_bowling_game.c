57ed832337ae20dc8b0da0af79e4b0116d4e4a23
makutak <takkouno@gmail.com>
2023-12-14 22:17:56 +0900

    各ロールで1本ずつ倒したときのテストを追加

--- kata/bowling_game/2023_12_10/tests/test_bowling_game.c
+++ kata/bowling_game/2023_12_10/tests/test_bowling_game.c
@@ -12,12 +12,22 @@ void test_all_gutters(void) {
   CU_ASSERT(bowling_game->score() == 0);
 }
 
+void test_all_ones(void) {
+  BowligGame *bowling_game = new_bowling_game();
+
+  for (int i = 0; i < 20; i++) {
+    bowling_game->roll(1);
+  }
+  CU_ASSERT(bowling_game->score() == 20);
+}
+
 int main() {
   CU_initialize_registry();
   CU_pSuite suite = CU_add_suite("BowlingGameTest", NULL, NULL);
 
   if (NULL != suite) {
     CU_add_test(suite, "test of all gutters", test_all_gutters);
+    CU_add_test(suite, "test of all ones", test_all_ones);
     // 他のテストも同様に追加
   }
 
