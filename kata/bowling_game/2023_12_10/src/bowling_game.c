4301b32946306a87b006a6453082fb22505d7e93
makutak <takkouno@gmail.com>
2023-12-15 01:34:02 +0900

    Cでは自己参照はできないので自身の構造体を引数で受け取るように

--- kata/bowling_game/2023_12_10/src/bowling_game.c
+++ kata/bowling_game/2023_12_10/src/bowling_game.c
@@ -7,17 +7,16 @@ void bowling_game_init() {
   // イニシャライザコード
 }
 
-void bowling_game_roll(int pins) {
-  // ピンを倒すコード
+void bowling_game_roll(BowlingGame* game, int pins) {
+  game->current_score += pins;
 }
 
-int bowling_game_score() {
-  return 0;
-}
+int bowling_game_score(BowlingGame* game) { return game->current_score; }
 
-BowligGame* new_bowling_game() {
-  BowligGame* game = malloc(sizeof(BowligGame));
+BowlingGame* new_bowling_game() {
+  BowlingGame* game = malloc(sizeof(BowlingGame));
   game->current_role = 0;
+  game->current_score = 0;
   game->roll = bowling_game_roll;
   game->score = bowling_game_score;
   return game;
--- kata/bowling_game/2023_12_10/src/bowling_game.h
+++ kata/bowling_game/2023_12_10/src/bowling_game.h
@@ -2,16 +2,16 @@
 #ifndef BOWLING_GAME_H
 #define BOWLING_GAME_H
 
-typedef struct BowligGame {
+typedef struct BowlingGame {
   int current_role;
-  int scores[21];
-  void (*roll)(int pins);
-  int (*score)();
-} BowligGame;
+  int current_score;
+  void (*roll)(struct BowlingGame*, int pins);
+  int (*score)(struct BowlingGame*);
+} BowlingGame;
 
 void bowling_game_init();
-void bowling_game_roll(int pins);
-int bowling_game_score();
-BowligGame* new_bowling_game();
+void bowling_game_roll(BowlingGame* bowling_game, int pins);
+int bowling_game_score(BowlingGame* bowling_game);
+BowlingGame* new_bowling_game();
 
 #endif  // BOWLING_GAME_H
