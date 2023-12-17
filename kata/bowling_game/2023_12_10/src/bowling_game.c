77e2bddef4170ef1c690163f5fd53df455f06923
makutak <takkouno@gmail.com>
2023-12-18 02:41:30 +0900

    ロールごとのスコアを記録するように修正

--- kata/bowling_game/2023_12_10/src/bowling_game.c
+++ kata/bowling_game/2023_12_10/src/bowling_game.c
@@ -2,6 +2,7 @@
 #include "bowling_game.h"
 
 #include <stdlib.h>
+#include <string.h>
 
 void bowling_game_init() {
   // イニシャライザコード
@@ -9,13 +10,22 @@ void bowling_game_init() {
 
 void bowling_game_roll(BowlingGame* game, int pins) {
   game->current_score += pins;
+  game->rolls[game->current_role++] = pins;
 }
 
-int bowling_game_score(BowlingGame* game) { return game->current_score; }
+int bowling_game_score(BowlingGame* game) {
+  int score = 0;
+  for (int i = 0; i < 20; i++) {
+    score += game->rolls[i];
+  }
+
+  return game->current_score;
+}
 
 BowlingGame* new_bowling_game() {
   BowlingGame* game = malloc(sizeof(BowlingGame));
   game->current_role = 0;
+  memset(game->rolls, 0, sizeof(game->rolls));
   game->current_score = 0;
   game->roll = bowling_game_roll;
   game->score = bowling_game_score;
