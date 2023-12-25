079545d0234ebaaaaefd67dfb4863c75d3ef4d8c
makutak <takkouno@gmail.com>
2023-12-26 01:06:29 +0900

    すべてのロールで1本倒したときのテスト

--- kata/bowling_game/python/2023_12_25/test_Game.py
+++ kata/bowling_game/python/2023_12_25/test_Game.py
@@ -8,6 +8,13 @@ class TestGame(unittest.TestCase):
             g.roll(0)
         self.assertEqual(0, g.score())
 
+    def test_all_ones(self):
+        g = Game()
+
+        for _ in range(0, 20):
+            g.roll(1)
+        self.assertEqual(20, g.score())
+
 
 if __name__ == '__main__':
     unittest.main()
