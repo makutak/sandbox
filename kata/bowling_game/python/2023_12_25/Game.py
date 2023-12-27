8fe85daf2e52cb0e2ca66ab71f995be8b35cd0ea
makutak <takkouno@gmail.com>
2023-12-27 20:01:35 +0900

    変数名修正

--- kata/bowling_game/python/2023_12_25/Game.py
+++ kata/bowling_game/python/2023_12_25/Game.py
@@ -9,13 +9,13 @@ class Game:
 
     def score(self):
         score = 0
-        i = 0
+        frame_index = 0
         for _ in range(10):
-            if (self._rolls[i] + self._rolls[i + 1] == 10): # spare
-                score += 10 + self._rolls[i + 2]
-                i += 2
+            if (self._rolls[frame_index] + self._rolls[frame_index + 1] == 10): # spare
+                score += 10 + self._rolls[frame_index + 2]
+                frame_index += 2
             else:
-                score += self._rolls[i] + self._rolls[i + 1]
-                i += 2
+                score += self._rolls[frame_index] + self._rolls[frame_index + 1]
+                frame_index += 2
 
         return score
