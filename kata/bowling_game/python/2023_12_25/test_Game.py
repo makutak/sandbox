617ed69572d3503d0e152d229f8beb36e34350c6
makutak <takkouno@gmail.com>
2023-12-27 18:45:37 +0900

    スペアのテストを追加

--- kata/bowling_game/python/2023_12_25/test_Game.py
+++ kata/bowling_game/python/2023_12_25/test_Game.py
@@ -17,6 +17,18 @@ class TestGame(unittest.TestCase):
         self.roll_many(20, 1)
         self.assertEqual(20, self.g.score())
 
+    def test_one_spare(self):
+        self.g.roll(5)
+        self.g.roll(5) # spare
+        self.roll_many(17, 0);
+        self.assertEqual(16, self.g.score())
+
+    def test_one_spare_2(self):
+        self.g.roll(6)
+        self.g.roll(4) # spare
+        self.roll_many(17, 0);
+        self.assertEqual(16, self.g.score())
+
 
 if __name__ == '__main__':
     unittest.main()
