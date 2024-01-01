1ede733cd466bb3a56ae5bed5508054b46130f47
makutak <takkouno@gmail.com>
2024-01-01 20:21:39 +0900

    各ロールで一本ずつ倒したときのテスト

--- kata/bowling_game/go/2023_12-31/bowling_game_test.go
+++ kata/bowling_game/go/2023_12-31/bowling_game_test.go
@@ -11,3 +11,13 @@ func TestGutterGame(t *testing.T) {
 		t.Errorf("Expected score of 0 but got %d", game.Score())
 	}
 }
+
+func TestAllOnes(t *testing.T) {
+	game := NewGame()
+	for i := 0; i < 20; i++ {
+		game.Roll(1)
+	}
+	if game.Score() != 20 {
+		t.Errorf("Expected score of 20 but got %d", game.Score())
+	}
+}
