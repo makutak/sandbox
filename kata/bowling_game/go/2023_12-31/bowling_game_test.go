8b738376cfdf6376bf263c9c6b093e0875712b87
makutak <takkouno@gmail.com>
2024-01-02 17:18:01 +0900

    ループ処理をヘルパ関数に切り出した

--- kata/bowling_game/go/2023_12-31/bowling_game_test.go
+++ kata/bowling_game/go/2023_12-31/bowling_game_test.go
@@ -11,6 +11,12 @@ func setUp() {
 	g = NewGame()
 }
 
+func rollMany(n int, pins int) {
+	for i := 0; i < n; i++ {
+		g.Roll(pins)
+	}
+}
+
 func TestMain(m *testing.M) {
 	setUp()
 
@@ -18,19 +24,14 @@ func TestMain(m *testing.M) {
 }
 
 func TestGutterGame(t *testing.T) {
-	for i := 0; i < 20; i++ {
-		g.Roll(0)
-	}
-
+	rollMany(20, 0)
 	if g.Score() != 0 {
 		t.Errorf("Expected score of 0 but got %d", g.Score())
 	}
 }
 
 func TestAllOnes(t *testing.T) {
-	for i := 0; i < 20; i++ {
-		g.Roll(1)
-	}
+	rollMany(20, 1)
 
 	if g.Score() != 20 {
 		t.Errorf("Expected score of 20 but got %d", g.Score())
