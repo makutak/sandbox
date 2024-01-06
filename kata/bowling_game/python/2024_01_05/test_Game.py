770e2ff926e3a740fdaf895056db8f28acd490dd
makutak <takkouno@gmail.com>
2024-01-06 14:12:14 +0900

    1本ずつ倒したときのテストを追加

--- kata/bowling_game/python/2024_01_05/test_Game.py
+++ kata/bowling_game/python/2024_01_05/test_Game.py
@@ -7,3 +7,11 @@ def test_gutter_game():
         g.roll(0)
 
     assert g.score() == 0
+
+def test_all_ones():
+    g = Game()
+
+    for _ in range(20):
+        g.roll(1)
+
+    assert g.score() == 20
