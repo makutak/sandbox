28f44586456036912b7156bb1857b4eea9a61b42
makutak <takkouno@gmail.com>
2024-01-06 14:47:25 +0900

    重複処理をヘルパ関数に切り出し

--- kata/bowling_game/python/2024_01_05/test_Game.py
+++ kata/bowling_game/python/2024_01_05/test_Game.py
@@ -6,15 +6,17 @@ class TestBowlingGame:
         self._game = Game()
 
 
+    def roll_many(self, n, pins):
+        for _ in range(n):
+            self._game.roll(pins)
+
     def test_gutter_game(self):
-        for _ in range(20):
-            self._game.roll(0)
+        self.roll_many(20, 0)
 
         assert self._game.score() == 0
 
 
     def test_all_ones(self):
-        for _ in range(20):
-            self._game.roll(1)
+        self.roll_many(20, 1)
 
         assert self._game.score() == 20
