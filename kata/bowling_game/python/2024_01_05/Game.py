e1218ab5dc7e4a97adcb07034a4a1bb00c42cf0d
makutak <takkouno@gmail.com>
2024-01-07 14:59:37 +0900

    spareの実装

--- kata/bowling_game/python/2024_01_05/Game.py
+++ kata/bowling_game/python/2024_01_05/Game.py
@@ -1,9 +1,19 @@
 class Game:
     def __init__(self):
-        self._score = 0
+        self._rolls = []
 
     def roll(self, pins):
-        self._score += pins
+        self._rolls.append(pins)
 
     def score(self):
-        return self._score
+        score = 0
+        frame_index = 0
+        for _ in range(10):
+            if self._rolls[frame_index] + self._rolls[frame_index + 1] == 10:
+                score += 10 + self._rolls[frame_index + 2]
+                frame_index += 2
+            else:
+                score += self._rolls[frame_index] + self._rolls[frame_index + 1]
+                frame_index += 2
+
+        return score
