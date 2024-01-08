db2879da23114af239de48c034e974b34b8de57f
makutak <takkouno@gmail.com>
2024-01-08 16:00:08 +0900

    refactor

--- kata/bowling_game/python/2024_01_05/Game.py
+++ kata/bowling_game/python/2024_01_05/Game.py
@@ -9,19 +9,29 @@ class Game:
         score = 0
         frame_index = 0
         for _ in range(10):
-            if self._rolls[frame_index] == 10:
-                score += (
-                    10 + self._rolls[frame_index + 1] + self._rolls[frame_index + 2]
-                )
+            if self.is_strike(frame_index):
+                score += self.strike_bonus(frame_index)
                 frame_index += 1
             elif self.is_spare(frame_index):
-                score += 10 + self._rolls[frame_index + 2]
+                score += self.spare_bonus(frame_index)
                 frame_index += 2
             else:
-                score += self._rolls[frame_index] + self._rolls[frame_index + 1]
+                score += self.sum_of_balls_in_frame(frame_index)
                 frame_index += 2
 
         return score
 
+    def is_strike(self, frame_index):
+        return self._rolls[frame_index] == 10
+
     def is_spare(self, frame_index):
         return self._rolls[frame_index] + self._rolls[frame_index + 1] == 10
+
+    def strike_bonus(self, frame_index):
+        return 10 + self._rolls[frame_index + 1] + self._rolls[frame_index + 2]
+
+    def spare_bonus(self, frame_index):
+        return 10 + self._rolls[frame_index + 2]
+
+    def sum_of_balls_in_frame(self, frame_index):
+        return self._rolls[frame_index] + self._rolls[frame_index + 1]
