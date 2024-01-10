4736e1112219d748f2d6e4b9a92c528405a18c93
makutak <takkouno@gmail.com>
2024-01-11 04:40:24 +0900

    refactor

--- kata/bowling_game/cpp/2024_01_09/tests/BowlingGameTest.cpp
+++ kata/bowling_game/cpp/2024_01_09/tests/BowlingGameTest.cpp
@@ -5,19 +5,23 @@
 class BowlingGameTest : public ::testing::Test {
 protected:
   Game game;
+
+  void rollMany(int n, int pins) {
+    for (int i = 0; i < n; i++) {
+      game.roll(pins);
+    }
+  }
 };
 
 
 TEST_F(BowlingGameTest, AllGutterGame) {
-  for (int i = 0; i < 20; i++) {
-    game.roll(0);
-  }
+  rollMany(0, 20);
+
   ASSERT_EQ(0, game.getScore());
 }
 
 TEST_F(BowlingGameTest, AllOnes) {
-  for (int i = 0; i < 20; i++) {
-    game.roll(1);
-  }
+  rollMany(1, 20);
+
   ASSERT_EQ(20, game.getScore());
 }
