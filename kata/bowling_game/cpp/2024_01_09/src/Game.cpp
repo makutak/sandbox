d69c55fc894ba8c2caaa8fc271a91f1d74b2f03b
makutak <takkouno@gmail.com>
2024-01-13 02:17:21 +0900

    変数名を分かりやすく

--- kata/bowling_game/cpp/2024_01_09/src/Game.cpp
+++ kata/bowling_game/cpp/2024_01_09/src/Game.cpp
@@ -7,15 +7,16 @@ void Game::roll(int pins) {
 
 int Game::getScore() {
   int score = 0;
-  int i = 0;
+  int frameIndex = 0;
   for (int frame = 0; frame < 10; frame++) {
-    if (rolls[i] + rolls[i + 1] == 10) {
-      score += 10 + rolls[i + 2];
-      i += 2;
+    if (rolls[frameIndex] + rolls[frameIndex + 1] == 10) {
+      score += 10 + rolls[frameIndex + 2];
+      frameIndex += 2;
     } else {
-      score += rolls[i] + rolls[i + 1];
-      i += 2;
+      score += rolls[frameIndex] + rolls[frameIndex + 1];
+      frameIndex += 2;
     }
   }
+
   return score;
 }
