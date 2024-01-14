fc555e39c6751cda386a45c947ef1424ae6a2311
makutak <takkouno@gmail.com>
2024-01-14 14:09:42 +0900

    変数名修正

--- kata/bowling_game/rust/2024_01_13/bowling_game/src/lib.rs
+++ kata/bowling_game/rust/2024_01_13/bowling_game/src/lib.rs
@@ -18,16 +18,16 @@ impl Game {
 
     pub fn score(&self) -> u16 {
         let mut score = 0;
-        let mut i = 0;
+        let mut frame_index = 0;
 
         for _ in 0..10 {
             // spare
-            if self.rolls[i] + self.rolls[i + 1] == 10 {
-                score += 10 + self.rolls[i + 2];
-                i += 2;
+            if self.rolls[frame_index] + self.rolls[frame_index + 1] == 10 {
+                score += 10 + self.rolls[frame_index + 2];
+                frame_index += 2;
             } else {
-                score += self.rolls[i] + self.rolls[i + 1];
-                i += 2;
+                score += self.rolls[frame_index] + self.rolls[frame_index + 1];
+                frame_index += 2;
             }
         }
 
