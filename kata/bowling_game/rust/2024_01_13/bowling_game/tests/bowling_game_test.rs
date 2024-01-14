7361f99e9a9b2ce3d704fbb92324d1e089c5513d
makutak <takkouno@gmail.com>
2024-01-14 13:45:46 +0900

    テスト時の重複処理を関数化した

--- kata/bowling_game/rust/2024_01_13/bowling_game/tests/bowling_game_test.rs
+++ kata/bowling_game/rust/2024_01_13/bowling_game/tests/bowling_game_test.rs
@@ -4,21 +4,23 @@ use bowling_game::Game;
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_all_gutter_game() {
-        let mut game = Game::new();
-        for _ in 0..20 {
-            game.roll(0);
+    fn roll_many(game: &mut Game, n: u16, pins: u16) {
+        for _ in 0..n {
+            game.roll(pins)
         }
+    }
+
+    #[test]
+    fn test_gutter_game() {
+        let mut game = Game::new();
+        roll_many(&mut game, 20, 0);
         assert_eq!(game.score(), 0);
     }
 
     #[test]
     fn test_all_ones() {
         let mut game = Game::new();
-        for _ in 0..20 {
-            game.roll(1);
-        }
+        roll_many(&mut game, 20, 1);
         assert_eq!(game.score(), 20);
     }
 }
