745b4192782148cc3943b17cd279853d30864fe8
makutak <takkouno@gmail.com>
2024-01-14 14:07:35 +0900

    スペアの実装

--- kata/bowling_game/rust/2024_01_13/bowling_game/src/lib.rs
+++ kata/bowling_game/rust/2024_01_13/bowling_game/src/lib.rs
@@ -1,17 +1,36 @@
 pub struct Game {
-    score: u16,
+    rolls: [u16; 21],
+    current_roll: usize,
 }
 
 impl Game {
     pub fn new() -> Self {
-        Game { score: 0 }
+        Game {
+            rolls: [0; 21],
+            current_roll: 0,
+        }
     }
 
     pub fn roll(&mut self, pins: u16) {
-        self.score += pins;
+        self.rolls[self.current_roll] = pins;
+        self.current_roll += 1;
     }
 
     pub fn score(&self) -> u16 {
-        self.score
+        let mut score = 0;
+        let mut i = 0;
+
+        for _ in 0..10 {
+            // spare
+            if self.rolls[i] + self.rolls[i + 1] == 10 {
+                score += 10 + self.rolls[i + 2];
+                i += 2;
+            } else {
+                score += self.rolls[i] + self.rolls[i + 1];
+                i += 2;
+            }
+        }
+
+        score
     }
 }
