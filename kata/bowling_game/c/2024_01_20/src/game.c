52f475cc33fcdcd1a3a8e3f6a3b710f79b2bce6f
makutak <takkouno@gmail.com>
2024-01-24 10:34:31 +0900

    fix variable name

--- kata/bowling_game/c/2024_01_20/src/game.c
+++ kata/bowling_game/c/2024_01_20/src/game.c
@@ -9,15 +9,15 @@ void game_roll(Game *game, int pins) {
 
 int game_get_score(Game *game) {
   int score = 0;
-  int i = 0;
+  int frame_index = 0;
 
   for (int frame = 0; frame < 10; frame++) {
-    if (game->rolls[i] + game->rolls[i + 1] == 10) {
-      score += 10 + game->rolls[i + 2];
-      i += 2;
+    if (game->rolls[frame_index] + game->rolls[frame_index + 1] == 10) {
+      score += 10 + game->rolls[frame_index + 2];
+      frame_index += 2;
     } else {
-      score += game->rolls[i] + game->rolls[i + 1];
-      i += 2;
+      score += game->rolls[frame_index] + game->rolls[frame_index + 1];
+      frame_index += 2;
     }
   }
 
