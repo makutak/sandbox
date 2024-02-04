fc0ea7ce1515325b522b9d59422c703e3bdd48ae
makutak <takkouno@gmail.com>
2024-02-04 10:34:31 +0900

    add all ones test

--- kata/bowling_game/go/2024_02_03/bowling_game_test.go
+++ kata/bowling_game/go/2024_02_03/bowling_game_test.go
@@ -13,3 +13,15 @@ func TestGutterGame(t *testing.T) {
 		t.Errorf("Expected score of 0, but got %d", game.Score())
 	}
 }
+
+func TestAllOnes(t *testing.T) {
+	game := NewGame()
+
+	for i := 0; i < 20; i++ {
+		game.Roll(1)
+	}
+
+	if game.Score() != 20 {
+		t.Errorf("Expected score of 20, but got %d", game.Score())
+	}
+}
