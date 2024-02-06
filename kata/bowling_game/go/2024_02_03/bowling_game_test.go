7bbf3267ea3a46b2c91a3fbc576e7b2296669fed
makutak <takkouno@gmail.com>
2024-02-07 00:57:49 +0900

    fix loop duplicate

--- kata/bowling_game/go/2024_02_03/bowling_game_test.go
+++ kata/bowling_game/go/2024_02_03/bowling_game_test.go
@@ -8,12 +8,16 @@ func setUp() {
 	game = NewGame()
 }
 
+func rollMany(n int, pins int) {
+	for i := 0; i < n; i++ {
+		game.Roll(pins)
+	}
+}
+
 func TestGutterGame(t *testing.T) {
 	setUp()
 
-	for i := 0; i < 20; i++ {
-		game.Roll(0)
-	}
+	rollMany(20, 0)
 
 	if game.Score() != 0 {
 		t.Errorf("Expected score of 0, but got %d", game.Score())
@@ -23,9 +27,7 @@ func TestGutterGame(t *testing.T) {
 func TestAllOnes(t *testing.T) {
 	setUp()
 
-	for i := 0; i < 20; i++ {
-		game.Roll(1)
-	}
+	rollMany(20, 1)
 
 	if game.Score() != 20 {
 		t.Errorf("Expected score of 20, but got %d", game.Score())
