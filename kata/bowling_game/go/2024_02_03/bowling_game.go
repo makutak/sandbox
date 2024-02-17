bb81dcccbf87496b11d0684e2479e7b942720ff1
makutak <takkouno@gmail.com>
2024-02-17 10:34:31 +0900

    remove comment

--- kata/bowling_game/go/2024_02_03/bowling_game.go
+++ kata/bowling_game/go/2024_02_03/bowling_game.go
@@ -20,7 +20,7 @@ func (g *Game) Score() int {
 	score := 0
 	frameIndex := 0
 	for frame := 0; frame < 10; frame++ {
-		if g.isStrile(frameIndex) { // strile
+		if g.isStrile(frameIndex) {
 			score += 10 + g.rolls[frameIndex+1] + g.rolls[frameIndex+2]
 			frameIndex += 1
 		} else if g.isSpare(frameIndex) {
