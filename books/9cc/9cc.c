6b615920b7c846563adc77b307aacf0fd9ac017d
makutak <takkouno@gmail.com>
2025-03-21 23:40:14 +0900

    新しいノードを作成する関数

--- books/9cc/9cc.c
+++ books/9cc/9cc.c
@@ -23,6 +23,22 @@ struct Node {
   int val;       // kindがNU_NUMの場合のみ使う
 };
 
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+
 // トークンの種類
 typedef enum {
   TK_RESERVED,  // 記号
