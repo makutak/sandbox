cb49a4a3c5790a0b956f1183f4230f2b661abcad
makutak <takkouno@gmail.com>
2025-04-11 22:12:20 +0900

    複数文字のローカル変数に対応

--- books/9cc/tokenize.c
+++ books/9cc/tokenize.c
@@ -2,6 +2,7 @@
 
 char *user_input;
 Token *token;
+LVar *locals;
 
 // エラーを報告するための関数
 // printfと同じ引数を取る
@@ -82,6 +83,14 @@ bool startswith(char *p, char *q) {
   return memcmp(p, q, strlen(q)) == 0;
 }
 
+bool is_alpha(char c) {
+  return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z');
+}
+
+bool is_alnum(char c) {
+  return is_alpha(c) || ('0' <= c && c <= '9') || (c == '_');
+}
+
 // 入力文字列pをトークナイズしてそれを返す
 Token *tokenize() {
   char *p = user_input;
@@ -101,8 +110,11 @@ Token *tokenize() {
       continue;
     }
 
-    if ('a' <= *p && *p <= 'z') {
-      cur = new_token(TK_IDENT, cur, p++, 1);
+    if (is_alpha(*p)) {
+      char *q = p;
+      while (is_alnum(*p))
+        p++;
+      cur = new_token(TK_IDENT, cur, q, p - q);
       continue;
     }
 
