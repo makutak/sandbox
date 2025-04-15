4dc7ca425b8cbb9ee3b0598d141b70b2f1d536d7
makutak <takkouno@gmail.com>
2025-04-16 02:14:44 +0900

    whileのテストを追加

--- books/9cc/test.sh
+++ books/9cc/test.sh
@@ -56,5 +56,7 @@ assert 14 'a = 3;b = 5 * 6 - 8;return a + b / 2;'
 
 assert 1 'if (1) return 1; else return 0;'
 assert 0 'if (1 == 0) return 1; else return 0;'
+assert 10 'i=0; while(i<10) i=i+1; return i;'
+assert 11 'i=0; while(i<=10) i=i+1; return i;'
 
 echo OK
