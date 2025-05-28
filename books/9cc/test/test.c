9d7d1e4619a780b47cb17c7f6a60642e1f444c04
makutak <takkouno@gmail.com>
2025-05-28 21:45:13 +0900

    四則演算テスト移行

--- books/9cc/test/test.c
+++ books/9cc/test/test.c
@@ -9,5 +9,15 @@ int assert(int expected, int actual, char *code) {
 
 int main() {
   assert(0, 0, "0");
+  assert(42, 42, "42");
+  assert(21, 5 + 20 - 4, "5+20-4");
+  assert(41, 12 + 34 - 5, "12 + 34 - 5");
+  assert(47, 5 + 6 * 7, "5+6*7");
+  assert(15, 5 * (9 - 6), "5*(9-6)");
+  assert(4, (3 + 5) / 2, "(3+5)/2");
+  assert(10, -10 + 20, "-10 + 20");
+  assert(10, - -10, "- -10");
+  assert(10, - -+10, "- - +10");
+
   return 0;
 }
