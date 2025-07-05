9a2c2d9514e808431061d893da54a0c50440bdbf
makutak <takkouno@gmail.com>
2025-07-05 22:12:20 +0900

    test all ones

--- kata/bowling_game/python/2025_07_08/test_bowling_game.py
+++ kata/bowling_game/python/2025_07_08/test_bowling_game.py
@@ -10,3 +10,8 @@ class BowlingGameTest(unittest.TestCase):
         for _ in range(20):
             self.game.roll(0)
         self.assertEqual(0, self.game.score())
+
+    def test_all_one(self):
+        for _ in range(20):
+            self.game.roll(1)
+        self.assertEqual(20, self.game.score())
