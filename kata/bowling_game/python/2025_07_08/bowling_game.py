77fbe2e00f63c059487adc2555735415d971d999
makutak <takkouno@gmail.com>
2025-07-08 01:54:45 +0900

    test one spare

--- kata/bowling_game/python/2025_07_08/bowling_game.py
+++ kata/bowling_game/python/2025_07_08/bowling_game.py
@@ -1,9 +1,20 @@
 class Game:
     def __init__(self):
-        self.total_score = 0
+        self.rolls = []
 
     def roll(self, pins):
-        self.total_score += pins
+        self.rolls.append(pins)
 
     def score(self):
-        return self.total_score
+        score = 0
+        frame_index = 0
+
+        for frame in range(10):
+            if self.rolls[frame_index] + self.rolls[frame_index + 1] == 10:
+                score += 10 + self.rolls[frame_index + 2]
+                frame_index += 2
+            else:
+                score += self.rolls[frame_index] + self.rolls[frame_index + 1]
+                frame_index += 2
+
+        return score
