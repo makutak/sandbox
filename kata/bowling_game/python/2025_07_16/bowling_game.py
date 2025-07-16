3a93cc4f92713fac725adc9e6985fcc51fce8d22
makutak <takkouno@gmail.com>
2025-07-17 01:54:45 +0900

    test gutter game

--- kata/bowling_game/python/2025_07_16/bowling_game.py
+++ kata/bowling_game/python/2025_07_16/bowling_game.py
@@ -1,3 +1,10 @@
 class Game:
     def __init__(self):
         pass
+
+
+    def roll(self, pins):
+        pass
+
+    def score(self):
+        return 0
--- kata/bowling_game/python/2025_07_16/test_bowling_game.py
+++ kata/bowling_game/python/2025_07_16/test_bowling_game.py
@@ -4,3 +4,8 @@ from bowling_game import Game
 class BowlingGameTest(unittest.TestCase):
     def test_gutter_game(self):
         game = Game()
+
+        for _ in range(20):
+            game.roll(0)
+
+        self.assertEqual(0, game.score())
