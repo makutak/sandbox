ea5bbf9a801ef6e4875d5a57b4af0bf1ac06d77d
makutak <takkouno@gmail.com>
2025-07-18 13:08:35 +0900

    test one spare

--- kata/bowling_game/python/2025_07_16/test_bowling_game.py
+++ kata/bowling_game/python/2025_07_16/test_bowling_game.py
@@ -7,13 +7,18 @@ class BowlingGameTest(unittest.TestCase):
 
     def test_gutter_game(self):
         self._roll_many(20, 0)
-
         self.assertEqual(0, self.game.score())
 
     def test_all_ones(self):
         self._roll_many(20, 1)
-
         self.assertEqual(20, self.game.score())
+
+    def test_one_spare(self):
+        self.game.roll(5)
+        self.game.roll(5)
+        self.game.roll(3)
+        self._roll_many(17,0)
+        self.assertEqual(16, self.game.score())
 
     def _roll_many(self, n, pins):
         for _ in range(n):
