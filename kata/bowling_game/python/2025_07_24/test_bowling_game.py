47fd16f09faa4133828ca3f68d653608420961a5
makutak <takkouno@gmail.com>
2025-07-26 18:50:40 +0900

    refactor

--- kata/bowling_game/python/2025_07_24/test_bowling_game.py
+++ kata/bowling_game/python/2025_07_24/test_bowling_game.py
@@ -3,14 +3,17 @@ from bowling_game import Game
 
 
 class BowlingGameTest(unittest.TestCase):
+    def setUp(self):
+        self.game = Game()
+
     def test_gutter_game(self):
-        game = Game()
-        for _ in range(20):
-            game.roll(0)
-        self.assertEqual(0, game.score())
+        self._roll_many(20, 0)
+        self.assertEqual(0, self.game.score())
 
     def test_all_ones(self):
-        game = Game()
-        for _ in range(20):
-            game.roll(1)
-        self.assertEqual(20, game.score())
+        self._roll_many(20, 1)
+        self.assertEqual(20, self.game.score())
+
+    def _roll_many(self, n, pins):
+        for _ in range(n):
+            self.game.roll(pins)
