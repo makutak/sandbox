0708de1971febda12ebeb976221385d585ada243
makutak <takkouno@gmail.com>
2026-01-21 22:12:20 +0900

    add comment

--- labo/networks/tap-device/tap.c
+++ labo/networks/tap-device/tap.c
@@ -11,25 +11,34 @@
 #define CLONE_DEVICE "/dev/net/tun"
 
 int main(int argc, char *argv[]) {
-  int fd;
-  struct ifreq ifr = {};
-  char buf[2046];
+  int fd;                // ファイルディスクリプタ
+  struct ifreq ifr = {}; // インターフェース設定用構造体
+  char buf[2046];        // 受信パケットを一時的に格納するバッファ(MTU + 余裕)
   char *ifname;
-  ssize_t n;
+  ssize_t n; // システムコールが読み込んだバイト数を格納する
 
+  // 実行時にインターフェース名が渡されなければエラー
   if (argc != 2) {
     fprintf(stderr, "usage: %s ifname\n", argv[0]);
     return -1;
   }
 
+  // /dev/net/tunを読み書きモードで開く
   fd = open(CLONE_DEVICE, O_RDWR);
   if (fd == -1) {
     perror("open");
     return -1;
   }
 
+  // 仮想デバイスの生成
+
+  // コマンドライン引数で指定された名前を設定用構造体の名前フィールドにコピーする
   strncpy(ifr.ifr_name, argv[1], sizeof(ifr.ifr_name) - 1);
+  // 動作モードの指定
+  // - IFF_TAP: L2(イーサネット)モード。イーサネットヘッダを含むフレーム全体を扱う
+  // - IFF_NO_PI: No Packet Information 4バイトのパケット情報ヘッダ(Flags/Proto)を付けない設定
   ifr.ifr_flags = IFF_TAP | IFF_NO_PI;
+  // fdに対してTAPインターフェースを割り当てる
   if (ioctl(fd, TUNSETIFF, &ifr) == -1) {
     perror("ioctl [TUNSETIFF]");
     close(fd);
@@ -37,9 +46,10 @@ int main(int argc, char *argv[]) {
   }
 
   ifname = ifr.ifr_name;
+  // カーネルによって割り当てられた正式なインターフェース名を表示する
   fprintf(stderr, "ifname: %s\n", ifname);
   while (1) {
-    n = read(fd, buf, sizeof(buf));
+    n = read(fd, buf, sizeof(buf)); // TAPデバイスにパケットが到達するまで待機
     if (n == -1) {
       if (errno == EINTR) {
         continue;
@@ -48,6 +58,7 @@ int main(int argc, char *argv[]) {
       close(fd);
       return -1;
     }
+    // 受信に成功したら受信したバイト数とインターフェース名を表示する
     printf("recv: %zd via %s\n", n, ifname);
   }
   close(fd);
